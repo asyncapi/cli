bcea3bb3dfeec430ca2f38cc87be30f325cb1d3e
Jorge Aguiar Martin <jotamusik@gmail.com>
2021-05-28 15:14:25 +0100

    test: CommandsRouter tests implemented

--- src/CommandsRouter.spec.tsx
+++ src/CommandsRouter.spec.tsx
@@ -1,13 +1,53 @@
 import { render } from 'ink-testing-library';
 import { commandsRouter } from "./CommandsRouter";
+import { SpecificationFile } from "./hooks/validation";
+import chalk from "chalk";
 
 function renderAux(cli: any) {
   return render(commandsRouter(cli));
 }
 
 describe('CommandsRouter should', () => {
-  xtest('route to help component', () => {
-    const { lastFrame } = renderAux({});
-    expect(lastFrame).toBe(null);
+  test('route to help component when help command passed', () => {
+    const { lastFrame } = renderAux({
+      input: ['help'],
+      help: 'Help Message',
+      flags: {
+        context: '',
+        watch: false,
+      },
+    });
+    expect(lastFrame()).toBe('Help Message');
+  });
+
+  test('route to help component when no command passed', () => {
+    const { lastFrame } = renderAux({
+      input: [],
+      help: 'Help Message',
+      flags: {
+        context: '',
+        watch: false,
+      },
+    });
+    expect(lastFrame()).toBe('Help Message');
+  });
+
+  test('route to validate component when validate command passed', (done) => {
+    const { lastFrame } = renderAux({
+      input: ['validate'],
+      help: 'Help Message',
+      flags: {
+        context: 'test/specification.yml',
+        watch: false,
+      },
+    });
+
+    // NOTE: This is needed to be able to match the test result file name due to its dependency with the file system
+    const file = new SpecificationFile('test/specification.yml');
+
+    setTimeout(() => {
+      expect(lastFrame()).toBe(chalk.green(`File: ${file.getFileName()} - was validated successfully!`));
+      done();
+    }, 200);
   });
 });
