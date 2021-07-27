89a5881bc3b40afceea0f8099b9132634d31c6aa
Souvik <souvikde.ns@gmail.com>
2021-07-27 20:27:46 +0530

    feat: created help command constant

--- src/messages.tsx
+++ src/messages.tsx
@@ -9,3 +9,40 @@ export const DELETING_CURRENT_CONTEXT = 'You are tyring to delete a context that
 export const MISSING_CURRENT_CONTEXT = 'No context is set as current, please set a current context.';
 
 export const MISSING_ARGUMENTS = 'Missing arguments.';
+
+const HELP_FLAG = '-h, --help  display help for command';
+
+export const Help = {
+  root: {
+    usage: 'asyncapi [options] [command]',
+    shortHelp: '',
+    options: [
+      '-v, --version  output the version number',
+      HELP_FLAG
+    ]
+  },
+  validate: {
+    usage: 'asyncapi validate [options]',
+    options: [
+      '-f, --file <spec-path>  Path of the spec file',
+      '-c, --context <saved-context-name>  context name to use',
+      '-w, --watch  watch mode',
+      HELP_FLAG
+    ]
+  },
+  context: {
+    usage: 'asyncapi context [options] [command]',
+    shortDesc: 'Manage contexts',
+    longDesc: 'Store key-value pair of contexts and the CLI will load your contexts \n automatically making your commands shorter',
+    options: [
+      HELP_FLAG
+    ],
+    commands: [
+      'list  list all saved commands',
+      'current  display current context in use',
+      'use <context-name>  set context as current context',
+      'add <context-name> <spec-path>  add/update context',
+      'remove <context-name> remove a context'
+    ]
+  }
+};
