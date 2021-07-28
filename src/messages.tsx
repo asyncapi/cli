2bb4ebdd236be2e98a9c9be12d7bc9da61f29b82
Souvik <souvikde.ns@gmail.com>
2021-07-28 13:08:48 +0530

    feat: created help class to generate help messages

--- src/messages.tsx
+++ src/messages.tsx
@@ -10,39 +10,69 @@ export const MISSING_CURRENT_CONTEXT = 'No context is set as current, please set
 
 export const MISSING_ARGUMENTS = 'Missing arguments.';
 
-const HELP_FLAG = '-h, --help  display help for command';
+export interface CommandHelp {
+  name: string,
+  usage: string,
+  shortDescription?: string,
+  longDescription?: string,
+  flags?: string[],
+  commands?: string[]
+}
 
-export const Help = {
-  root: {
+export class Help {
+  private _helpFlag = '-h, --help  display help for command';
+  private root: CommandHelp = {
+    name: 'root',
     usage: 'asyncapi [options] [command]',
-    options: [
-      '-v, --version  output the version number',
-      HELP_FLAG
-    ]
-  },
-  validate: {
-    usage: 'asyncapi validate [options]',
-    shortDesc: 'Validate an AsyncApi file',
-    options: [
-      '-f, --file <spec-path>  Path of the spec file',
-      '-c, --context <saved-context-name>  context name to use',
-      '-w, --watch  watch mode',
-      HELP_FLAG
-    ]
-  },
-  context: {
-    usage: 'asyncapi context [options] [command]',
-    shortDesc: 'Manage contexts',
-    longDesc: 'Store key-value pair of contexts and the CLI will load your contexts \n automatically making your commands shorter',
-    options: [
-      HELP_FLAG
-    ],
-    commands: [
-      'list  list all saved commands',
-      'current  display current context in use',
-      'use <context-name>  set context as current context',
-      'add <context-name> <spec-path>  add/update context',
-      'remove <context-name> remove a context'
+    flags: [
+      this._helpFlag,
+      '-v, -version  output the version number'
     ]
   }
-};
+
+  private validate: CommandHelp = {
+    name: 'validate',
+    usage: 'asyncapi validate [options]',
+    shortDescription: 'Validate an asyncapi file',
+    flags: [
+      '-f, --file <spec-file-path>  Path of the spec file',
+      '-c, --context <saved-context-name>  Context name to use',
+      '-w, --watch  watch mode',
+      this._helpFlag
+    ]
+  }
+
+  private context: CommandHelp = {
+    name: 'context',
+    usage: 'asyncapi context [options] [command]',
+    shortDescription: 'Manage contexts',
+    flags: [
+      this._helpFlag
+    ],
+    commands: [
+      'list  list all saved contexts',
+      'current  see current context',
+      'use <context-name>  set a context as current',
+      'add <context-name> <spec-file-path>  add/update context',
+      'remove <context-name>  remove a context'
+    ]
+  }
+
+  rootHelp(): string {
+    let helpString = '';
+    helpString += `usage: ${this.root.usage}\n\n`;
+    helpString += 'options:\n';
+    if (this.root.flags) {
+      for (const flag of this.root.flags) {
+        helpString += ` ${flag} \n`;
+      }
+    }
+
+    helpString += '\n';
+
+    helpString += 'commads:\n';
+    helpString += ` context [options] [command]  ${this.context.shortDescription} \n`;
+    helpString += ` validate [options]  ${this.validate.shortDescription}\n`;
+    return helpString;
+  }
+}
