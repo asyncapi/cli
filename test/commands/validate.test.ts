30ec37b3b30c1b060db3317e4154c160999913e1
Souvik <souvikde.ns@gmail.com>
2021-10-19 16:01:31 +0530

    test: fixed issue with setup but still tests not running as intended

--- test/commands/validate.test.ts
+++ test/commands/validate.test.ts
@@ -1,11 +1,12 @@
 import 'reflect-metadata';
-import {test} from '@oclif/test';
+import {test, expect} from '@oclif/test';
 
 describe('Validate Command ', () => {
   test
     .stdout()
-    .command(['validate ./test/specification.yml'])
-    .it('should validate successfully', ctx => {
-      console.log(ctx.stdout);
+    .command(['validate', './test/specification.yml'])
+    .it('should validate successfully', (ctx) => {
+      console.log(ctx);
+      expect(ctx.stdout).to.contain('');
     });
 });
