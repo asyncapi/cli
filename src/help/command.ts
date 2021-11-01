e3debf2750f01e5383272757fdf714ef05681de6
Souvikns <souvikde.ns@gmail.com>
2021-11-01 15:28:10 +0530

    refactor: lint fixes

--- src/help/command.ts
+++ src/help/command.ts
@@ -7,53 +7,52 @@ import indent = require('indent-string')
 const {
   underline,
   bold,
-} = chalk
-let {
+} = chalk;
+const {
   dim,
-} = chalk
+} = chalk;
 
 export default class CommandHelper extends CommandHelp {
+  flags(flags: Config.Command.Flag[]): string | undefined {
+    if (flags.length === 0) {return;}
+    const body = renderList(flags.map(flag => {
+      let left = flag.helpLabel;
 
-    flags(flags: Config.Command.Flag[]): string | undefined {
-        if (flags.length === 0) return
-        const body = renderList(flags.map(flag => {
-            let left = flag.helpLabel
+      if (!left) {
+        const label = [];
+        if (flag.char) {label.push(`-${flag.char[0]}`);}
+        if (flag.name) {
+          if (flag.type === 'boolean' && flag.allowNo) {
+            label.push(`--[no-]${flag.name.trim()}`);
+          } else {
+            label.push(`--${flag.name.trim()}`);
+          }
+        }
+        left = label.join(', ');
+      }
 
-            if (!left) {
-                const label = []
-                if (flag.char) label.push(`-${flag.char[0]}`)
-                if (flag.name) {
-                    if (flag.type === 'boolean' && flag.allowNo) {
-                        label.push(`--[no-]${flag.name.trim()}`)
-                    } else {
-                        label.push(`--${flag.name.trim()}`)
-                    }
-                }
-                left = label.join(', ')
-            }
+      if (flag.type === 'option') {
+        let value = flag.helpValue || flag.name;
+        if (!flag.helpValue && flag.options) {
+          value = flag.options.join('|');
+        }
+        if (!value.includes('|')) {value = underline(value);}
+        left += `=${value}`;
+      }
 
-            if (flag.type === 'option') {
-                let value = flag.helpValue || flag.name
-                if (!flag.helpValue && flag.options) {
-                    value = flag.options.join('|')
-                }
-                if (!value.includes('|')) value = underline(value)
-                left += `=${value}`
-            }
+      let right = flag.description || '';
+      // `flag.default` is not always a string (typing bug), hence `toString()`
+      if (flag.type === 'option' && (flag.default || flag.default?.toString() === '0')) {
+        right = `[default: ${flag.default}] ${right}`;
+      }
+      if (flag.required) {right = `(required) ${right}`;}
 
-            let right = flag.description || ''
-            // `flag.default` is not always a string (typing bug), hence `toString()`
-            if (flag.type === 'option' && (flag.default || flag.default?.toString() === '0')) {
-                right = `[default: ${flag.default}] ${right}`
-            }
-            if (flag.required) right = `(required) ${right}`
-
-            return [left, dim(right.trim())]
-        }), { stripAnsi: this.opts.stripAnsi, maxWidth: this.opts.maxWidth - 2 })
-        return [
-            bold('FLAGS'),
-            indent(body, 2),
-        ].join('\n')
-    }
+      return [left, dim(right.trim())];
+    }), { stripAnsi: this.opts.stripAnsi, maxWidth: this.opts.maxWidth - 2 });
+    return [
+      bold('FLAGS'),
+      indent(body, 2),
+    ].join('\n');
+  }
 }
 
