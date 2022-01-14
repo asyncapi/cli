be8a691085be767d00c123c0cba640470258b1c5
Pedro Ramos <pedroramosromero@gmail.com>
2022-01-14 14:14:04 +0100

    New try-catch block added

--- src/commands/convert.ts
+++ src/commands/convert.ts
@@ -10,7 +10,7 @@ export default class Convert extends Command {
 
   static flags = {
     help: flags.help({ char: 'h' }),
-    'file-name': flags.string({ char: 'f', description: 'name of the file to convert' }),
+    'file-name': flags.string({ char: 'n', description: 'name of the file to convert' }),
     output: flags.string({ char: 'o', description: 'name of the file where the result is saved' }),
     'target-version': flags.string({ char: 't', description: 'asyncapi version to convert to' }),
     id: flags.string({ char: 'i', description: 'application id, if needed' })
@@ -24,7 +24,8 @@ export default class Convert extends Command {
     const { args } = this.parse(Convert);
     const filePath = args['spec-file'];
     let specFile;
-
+    
+    // LOAD FILE
     try {
       specFile = await load(filePath);
     } catch (err) {
@@ -37,6 +38,8 @@ export default class Convert extends Command {
         this.error(err as Error);
       }
     }
+
+    // VALIDATION
     try {
       if (specFile.getFilePath()) {
         await parser.parse(specFile.text());
@@ -51,30 +54,21 @@ export default class Convert extends Command {
         err: error
       });
     }
-
-    /*     
-    try {
-      if (specFile.getFilePath()) {
-        await parser.parse(specFile.text());
-        this.log(`File ${specFile.getFilePath()} successfully validated!`);
-      } else if (specFile.getFileURL()) {
-        await parser.parse(specFile.text());
-        this.log(`URL ${specFile.getFileURL()} successfully validated`);
-      }
-    } catch (error) {
-      throw new ValidationError({
-        type: 'parser-error',
-        err: error
-      });
-    } */
     
-    // Conversion
+    // CONVERSION
     try {
-      
-
+      /* if (specFile.getFilePath()) {
+        await parser.parse(specFile.text());
+        this.log(`File ${specFile.getFilePath()} successfully validated!`);
+      } else if (specFile.getFileURL()) {
+        await parser.parse(specFile.text());
+        this.log(`URL ${specFile.getFileURL()} successfully validated`);
+      } */
     } catch (error) {
-      
-      };
-    }
-
+/*       throw new ValidationError({
+        type: 'parser-error',
+        err: error
+      });
+ */ }
   }
+}
