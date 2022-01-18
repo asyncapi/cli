4c226a938782a13919fdc64e18afea89b818dc20
Pedro Ramos <pedroramosromero@gmail.com>
2022-01-18 13:38:17 +0100

    Try-catch blocks unified and convert function added

--- src/commands/convert.ts
+++ src/commands/convert.ts
@@ -4,13 +4,15 @@ import Command from '../base';
 import { ValidationError } from '../errors/validation-error';
 import { load } from '../models/SpecificationFile';
 import { SpecificationFileNotFound } from '../errors/specification-file';
+import { convert } from '@asyncapi/converter';
+import { Specification } from '../models/SpecificationFile';
 
 export default class Convert extends Command {
   static description = 'convert asyncapi documents older to newer versions';
 
   static flags = {
     help: flags.help({ char: 'h' }),
-    'file-name': flags.string({ char: 'n', description: 'name of the file to convert' }),
+    file: flags.string({ char: 'f', description: 'path to the file to convert' }),
     output: flags.string({ char: 'o', description: 'name of the file where the result is saved' }),
     'target-version': flags.string({ char: 't', description: 'asyncapi version to convert to' }),
     id: flags.string({ char: 'i', description: 'application id, if needed' })
@@ -24,10 +26,28 @@ export default class Convert extends Command {
     const { args } = this.parse(Convert);
     const filePath = args['spec-file'];
     let specFile;
+    let convertedFile;
 
-    // LOAD FILE
     try {
+      // LOAD FILE
       specFile = await load(filePath);
+
+      // VALIDATION
+      if (specFile.getFilePath()) {
+        await parser.parse(specFile.text());
+        this.log(`File ${specFile.getFilePath()} successfully validated!`);
+      } else if (specFile.getFileURL()) {
+        await parser.parse(specFile.text());
+        this.log(`URL ${specFile.getFileURL()} successfully validated!`);
+      }
+
+      // CONVERSION
+      convertedFile = await convert(specFile.text(), Convert.flags['target-version'], Convert.flags.id);
+      if (convertedFile.getFilePath()) {
+        this.log(`File ${convertedFile.getFilePath()} successfully converted!`);
+      } else if (convertedFile.getFileURL()) {
+        this.log(`URL ${convertedFile.getFileURL()} successfully converted!`);
+      }
     } catch (err) {
       if (err instanceof SpecificationFileNotFound) {
         this.error(new ValidationError({
@@ -37,42 +57,6 @@ export default class Convert extends Command {
       } else {
         this.error(err as Error);
       }
-    }
-
-    // VALIDATION
-    try {
-      if (specFile.getFilePath()) {
-        await parser.parse(specFile.text());
-        this.log(`File ${specFile.getFilePath()} successfully validated!`);
-      } else if (specFile.getFileURL()) {
-        await parser.parse(specFile.text());
-        this.log(`URL ${specFile.getFileURL()} successfully validated`);
-      }
-    } catch (error) {
-      throw new ValidationError({
-        type: 'parser-error',
-        err: error
-      });
-    }
-
-    // CONVERSION
-    try {
-      /* 
-      if (specFile.getFilePath()) {
-        await parser.parse(specFile.text());
-        this.log(`File ${specFile.getFilePath()} successfully validated!`);
-      } else if (specFile.getFileURL()) {
-        await parser.parse(specFile.text());
-        this.log(`URL ${specFile.getFileURL()} successfully validated`);
-        }
-      */
-    } catch (error) {
-      /*
-      throw new ValidationError({
-        type: 'parser-error',
-        err: error
-      });
-      */
     }
   }
 }
