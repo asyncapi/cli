2cf0aa542b714a01ab4d3a571b89825fe1d2d5b7
Abir <abir.pal899@gmail.com>
2022-02-09 16:14:22 +0530

    fix: refactor watcher configuration

--- src/globals.ts
+++ src/globals.ts
@@ -1,18 +1,19 @@
 import chokidar from 'chokidar';
 import chalk from 'chalk';
+import Command from '@oclif/command';
 
 const GreenLog = chalk.hex('#00FF00');
 const OrangeLog = chalk.hex('#FFA500');
 
-export const specWatcher = (filePath: string, handler: any, handlerName: string) => {
-  const CHOKIDAR_CONFIG = {
-    awaitWriteFinish: true
-  };
-  const WATCH_MESSAGES = {
-    logOnStart: (filePath: string) => console.log(GreenLog(`Watching AsyncAPI file at ${filePath}`)),
-    logOnChange: (handlerName: string) => console.log(OrangeLog(`Change detected, running ${handlerName}`)),
-  };
-  
+const CHOKIDAR_CONFIG = {
+  awaitWriteFinish: true
+};
+const WATCH_MESSAGES = {
+  logOnStart: (filePath: string) => console.log(GreenLog(`Watching AsyncAPI file at ${filePath}`)),
+  logOnChange: (handlerName: string) => console.log(OrangeLog(`Change detected, running ${handlerName}`)),
+};
+
+export const specWatcher = (filePath: string, handler: Command, handlerName: string) => {
   try {
     WATCH_MESSAGES.logOnStart(filePath);
     chokidar
