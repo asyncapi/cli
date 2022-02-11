4600880f5ccdea62af042d79b4f7a7990aaf75ee
Abir <abir.pal899@gmail.com>
2022-02-11 17:09:05 +0530

    fix: fix multiple watch instances

--- src/globals.ts
+++ src/globals.ts
@@ -6,33 +6,39 @@ import { Specification } from './models/SpecificationFile';
 const GreenLog = chalk.hex('#00FF00');
 const OrangeLog = chalk.hex('#FFA500');
 const CHOKIDAR_CONFIG = {
-  // awaitWriteFinish: true
+  // awaitWriteFinish: true // Used for large size specification files.
+
 };
 const WATCH_MESSAGES = {
   logOnStart: (filePath: string) => console.log(GreenLog(`Watching AsyncAPI file at ${filePath}\n`)),
   logOnChange: (handlerName: string) => console.log(OrangeLog(`Change detected, running ${handlerName}\n`)),
-  logOnAutoDisable: (docVersion: 'old' | 'new' | ''='') => console.log(OrangeLog(`Watch mode for ${docVersion} file was not enabled..\n`), OrangeLog('INFO: Watch works only with files from local file system\n'))
+  logOnAutoDisable: (docVersion: 'old' | 'new' | '' = '') => console.log(OrangeLog(`Watch mode for ${docVersion} file was not enabled..\n`), OrangeLog('INFO: Watch works only with files from local file system\n'))
 };
 
+let IS_CHOKIDAR_INSTANCE_RUNNING=false;
+
 export type specWatcherParams = {
-  spec:Specification,
-  handler:Command,
+  spec: Specification,
+  handler: Command,
   handlerName: string,
   docVersion?: 'old' | 'new' | ''
 }
 
-export const specWatcher = (params:specWatcherParams) => {
-  if (!params.spec.getFilePath()) {return WATCH_MESSAGES.logOnAutoDisable(params.docVersion);}
+export const specWatcher = (params: specWatcherParams) => {
+  if (!params.spec.getFilePath()) { return WATCH_MESSAGES.logOnAutoDisable(params.docVersion); }
 
   const filePath = params.spec.getFilePath() as string;
   try {
     WATCH_MESSAGES.logOnStart(filePath);
-    chokidar
-      .watch(filePath, CHOKIDAR_CONFIG)
-      .on('change', async () => {
-        WATCH_MESSAGES.logOnChange(params.handlerName);
-        await params.handler.run();
-      });
+    if (!IS_CHOKIDAR_INSTANCE_RUNNING) {
+      chokidar
+        .watch(filePath, CHOKIDAR_CONFIG)
+        .on('change', async () => {
+          WATCH_MESSAGES.logOnChange(params.handlerName);
+          await params.handler.run();
+        });
+      IS_CHOKIDAR_INSTANCE_RUNNING=true;
+    }
   } catch (error) {
     console.log(error);
   }
