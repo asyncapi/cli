0f221dd0f614a38b04148ea6aead6df96de61099
Samridhi-98 <agrawalsamriddhi83@gmail.com>
2022-02-12 20:00:24 +0530

    removed do section

--- test/commands/studio.test.ts
+++ test/commands/studio.test.ts
@@ -1,16 +1,14 @@
-import { expect, test } from '@oclif/test';
-import { start as startStudio } from '../../src/models/Studio';
-// import inquirer from 'inquirer';
+import { command, expect, test } from '@oclif/test';
+// import { WebSocket } from 'ws';
 
-//test web socket server connection
 describe('start', () => {
+    afterEach(() => {
+        // WebSocket.close();
+    });
     test
         .stderr()
         .stdout()
-        .do(() => {
-            startStudio('./test/specification.yml', 8080);
-        })
-        .command(['start', 'studio'])
+        .command(['start', 'studio', '--file=./test/specification.yml', '--port=8080'])
         .it('runs studio', ctx => {
             expect(ctx.stdout).to.contain('Studio is running at http://localhost:8080?liveServer=8080');
         });
