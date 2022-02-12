e47204535abddf597fc8cd4afcf957ee2bb29553
Samridhi-98 <agrawalsamriddhi83@gmail.com>
2022-02-12 18:46:24 +0530

    removed stub section

--- test/commands/new.test.ts
+++ test/commands/new.test.ts
@@ -1,17 +1,8 @@
 import { expect, test } from '@oclif/test';
-import inquirer from 'inquirer';
 
 describe('new', () => {
   test
     .stderr()
-    .stub(inquirer, 'prompt', () => {
-      return Promise.resolve({
-        filename: 'specification.yaml',
-        template: 'default-example.yaml',
-        studio: true,
-        port: 3000
-      });
-    })
     .stdout()
     .command(['new', '--no-tty', '-n=specification.yaml'])
     .it('runs new command', ctx => {
