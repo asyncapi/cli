ee85ddfdaf396dbe5fddd11e73300688d368643e
Souvikns <souvikde.ns@gmail.com>
2022-02-22 10:57:59 +0530

    chore: removing unused types

--- types.d.ts
+++ types.d.ts
@@ -2,8 +2,6 @@ declare module '@asyncapi/generator' {
     export default class Generator {
       constructor(template: string, outputdir: string, options?: any)
 
-      async generateFromFile(filepath: string)
-      async generateFromURL(url: string)
-      async generateFomString(asyncapiString: string, parserOptions: any = {})
+      generateFomString(asyncapiString: string, parserOptions?: any ): Promise<any>
     }
 }
