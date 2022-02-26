f3d8811c428a6e8fcba1d53b9e36fe1c56c810f7
Samridhi-98 <agrawalsamriddhi83@gmail.com>
2022-02-26 19:05:28 +0530

    minor fix

--- test/commands/new.test.ts
+++ test/commands/new.test.ts
@@ -5,8 +5,9 @@ describe('new', () => {
     .stderr()
     .stdout()
     .command(['new', '--no-tty', '-n=specification.yaml'])
-    .it('runs new command', (ctx, done) => {
-      expect(ctx.stdout).to.contain('specification.yaml');
+    .it('runs new command', (ctx,done) => {
+      expect(ctx.stderr).to.equals('');
+      expect(ctx.stdout).contains('specification.yaml');
       done();
     });
 });
