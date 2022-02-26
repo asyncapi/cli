55ba39187f38e08de543e4ddc4ecce2c9d198949
Samridhi-98 <agrawalsamriddhi83@gmail.com>
2022-02-26 13:47:07 +0530

    added stop studio in test

--- test/commands/studio.test.ts
+++ test/commands/studio.test.ts
@@ -1,15 +1,24 @@
-import { command, expect, test } from '@oclif/test';
-// import { WebSocket } from 'ws';
+import { expect, test } from '@oclif/test';
+import {execSync} from 'child_process';
 
 describe('start', () => {
-    afterEach(() => {
-        // WebSocket.close();
+  after(() => {
+    setTimeout(() => {
+      execSync('npx kill-port 8080');
+    }, 10000);
+  });   
+  test
+    .stderr()
+    .stdout()
+    .command(['start', 'studio', '--file=./test/specification.yml', '--port=8080'])
+    .it('runs studio', ctx => {
+      expect(ctx.stdout).to.contain('Studio is running at http://localhost:8080?liveServer=8080');
     });
-    test
-        .stderr()
-        .stdout()
-        .command(['start', 'studio', '--file=./test/specification.yml', '--port=8080'])
-        .it('runs studio', ctx => {
-            expect(ctx.stdout).to.contain('Studio is running at http://localhost:8080?liveServer=8080');
-        });
+
+  test
+    .stdout()
+    .command(['npm','run','stop'])
+    .catch(() => {
+      console.log('not working :(');
+    });   
 });
