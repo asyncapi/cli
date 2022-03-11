c5178e65cf716588fb1527194b8816ee61f2b9f9
alceil <ashishshajisrampickal@gmail.com>
2022-03-11 10:21:37 +0530

    added flags for optimize commands

--- src/commands/optimize.ts
+++ src/commands/optimize.ts
@@ -1,19 +1,31 @@
 import {Flags} from '@oclif/core';
 import Command from '../base';
-import * as optimizer from '@asyncapi/optimizer';
+import { Optimizer } from '@asyncapi/optimizer';
 
 import { load } from '../models/SpecificationFile';
 
 export default class Optimize extends Command {
-  static description = 'starts a new local instance of Studio';
+  static description = 'It enables users to optimize single AsyncAPI file';
 
   static flags = {
     help: Flags.help({ char: 'h' }),
+    report: Flags.boolean({ char: 'r', description: 'generates report' }),
   }
 
-  static args = []
+  static args = [
+    { name: 'file', description: 'context,filepath or url', required: true },
+  ]
 
   async run() {
     const { args,flags } = await this.parse(Optimize);
+    const filePath = args['file'];
+    const file = await load(filePath);
+    const optimizer = new Optimizer(file);
+    const report = optimizer.getOptimizedDocument();
+    this.log(report);
+    if (flags.report) {
+      const report = await optimizer.getReport();
+      this.log(String(report));
+    }
   }
 }
