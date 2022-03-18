0dfce9d95be54c68c99edf4f9c9ffaf4b2508f98
unknown <Amanpreetsinghbedi_19012722.cse@geu.ac.in>
2022-03-19 00:07:34 +0530

    Bundle asyncapi command added

---
+++ src/commands/bundle.ts
@@ -0,0 +1,71 @@
+import Command from '../base';
+import { Flags } from '@oclif/core';
+import { load } from '../models/SpecificationFile';
+import { outputFlag } from '../flags';
+import path  from 'path'; 
+import fs from 'fs';
+// @ts-ignore
+import yaml from 'js-yaml';
+// @ts-ignore
+import bundler from '@asyncapi/bundler';
+
+export default class Bundle extends Command{
+    static description = 'It enables users to bundle/merge specification into one';
+
+    static flags={
+        help: Flags.help({char : 'h'}),
+        output: outputFlag,
+    };
+    
+    // To input multiple files by disabling argument validation
+    static strict = false;
+
+    async run(){
+        const {argv,flags} = await this.parse(Bundle);
+        const specFiles = await loadSpecFilesfromFilePaths(argv);
+        const documents = await bundler(specFiles);
+        
+        // checking if output flag is passed or not as output flag is treated as object
+        if(Object.keys(flags).length === 0 && flags.constructor === Object){
+            this.log(documents.yml());
+        } else{
+            const outputFileLocation = flags['output'];
+            const outputFileExtension = path.extname(outputFileLocation!);
+    
+            if(outputFileExtension ==='.yaml' || outputFileExtension ==='.yml'){
+                fs.writeFile(outputFileLocation!, yaml.dump(documents.yml()), (err) => {
+                    if (err) {
+                        this.error(err);
+                    }
+                });
+
+            } else if(outputFileExtension ==='.json'){
+                fs.writeFile(outputFileLocation!, JSON.stringify((documents.json()),null,2),'utf8',(err) => {
+                    if (err) {
+                        this.error(err);
+                    }
+                });
+
+            } else if(outputFileExtension === '.txt'){
+                fs.writeFile(outputFileLocation!, (documents.string()),'utf8', (err) => {
+                    if (err) {
+                        this.error(err);
+                    }
+                });
+            } else{
+                this.error('Output file must be yaml,json or txt');
+            }
+
+        }
+    } 
+}
+
+// utility function to return an array of loaded files from their file paths.
+async function loadSpecFilesfromFilePaths(filePaths:string[]){
+
+    const specFilesPromises = filePaths.map(async filePath=>{
+        const specFile = await load(filePath); 
+        return specFile.text();
+    }); 
+    return await Promise.all(specFilesPromises);
+} 
--- src/flags.ts
+++ src/flags.ts
@@ -4,3 +4,8 @@ export const watchFlag = Flags.boolean({
   char: 'w',
   description: 'Enable watch mode'
 });
+
+export const outputFlag = Flags.string({
+  char: 'o',
+  description: 'Path to the output file'
+})
