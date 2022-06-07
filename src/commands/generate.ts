426c3611e9c739aa819ef8408f75c00e559655a0
Souvikns <souvikde.ns@gmail.com>
2022-06-07 16:25:34 +0530

    refactor: update cli help messages

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -26,7 +26,7 @@ interface ParsedFlags {
 }
 
 export default class Generate extends Command {
-  static description = 'Generator is a tool that you can use to generate whatever you want basing on the AsyncAPI specification file as an input.';
+  static description = 'generate whatever you want using templates compatible with AsyncAPI Generator';
 
   static examples: Example[] = [
     'asyncapi generate asyncapi.yaml @asyncapi/html-template'
@@ -75,8 +75,8 @@ export default class Generate extends Command {
   }
 
   static args = [
-    { name: 'asyncapi', description: 'Local path or url pointing to AsyncAPI specification file', required: true },
-    { name: 'template', description: 'Name of the generator template like for example @asyncapi/html-template or https://github.com/asyncapi/html-template', required: true }
+    { name: 'asyncapi', description: '- Local path or url pointing to AsyncAPI file', required: true },
+    { name: 'template', description: '- Name of the generator template like for example @asyncapi/html-template or https://github.com/asyncapi/html-template', required: true }
   ]
 
   async run() {
