6cc971aba9c4307f09917e217120ebb1f932d7b6
Jonas Lagoni <jonas-lt@live.dk>
2022-06-22 13:35:29 +0200

    added in-memory generation

--- src/commands/generate/types.ts
+++ src/commands/generate/types.ts
@@ -1,27 +1,40 @@
-import { CSharpFileGenerator, JavaFileGenerator, JavaScriptFileGenerator, TypeScriptFileGenerator, GoFileGenerator, Logger} from '@asyncapi/modelina';
+import { CSharpFileGenerator, JavaFileGenerator, JavaScriptFileGenerator, TypeScriptFileGenerator, GoFileGenerator, Logger, DartFileGenerator} from '@asyncapi/modelina';
 import { Flags } from '@oclif/core';
 import Command from '../../base';
 import { load } from '../../models/SpecificationFile';
 import { parse } from '@asyncapi/parser';
+enum Languages {
+  typescript = 'typescript',
+  csharp = 'csharp',
+  golang = 'golang',
+  java = 'java',
+  javascript = 'javascript',
+  dart = 'dart'
+}
 export default class Types extends Command {
   static description = 'Generates typed models';
 
   static args = [
-    { name: 'language', description: 'language output', options: ['typescript', 'csharp', 'golang', 'java', 'javascript'], required: true},
+    { 
+      name: 'language', 
+      description: 'language output', 
+      options: Object.keys(Languages), 
+      required: true 
+    },
     { name: 'file', description: 'spec path, url, or context-name', required: true },
   ]
 
   static flags = {
     help: Flags.help({ char: 'h' }),
-    output: Flags.string({ char: 'o', description: 'output path to write the types to', required: true}),
+    output: Flags.string({ char: 'o', description: 'output path to write the model to' }),
     /**
      * Go and Java specific package name to use for the generated types
      */
-    packageName: Flags.string({description: 'Go and Java specific, define the package to use for the generated types', required: false}),
+    packageName: Flags.string({ description: 'Go and Java specific, define the package to use for the generated types', required: false }),
     /**
      * C# specific options
      */
-    namespace: Flags.string({description: 'C# specific, define the namespace to use for the generated types', required: false}),
+    namespace: Flags.string({ description: 'C# specific, define the namespace to use for the generated types', required: false }),
   }
 
   async run() {
@@ -30,13 +43,19 @@ export default class Types extends Command {
     const { language, file } = passedArguments.args;
     const inputFile = await load(file) || await load();
     const parsedInput = await parse(inputFile.text());
+    Logger.setLogger({
+      info: this.log,
+      debug: this.debug,
+      warn: this.warn,
+      error: this.error,
+    });
     let fileGenerator;
     let fileOptions = {};
     switch (language) {
-    case 'typescript':
+    case Languages.typescript:
       fileGenerator = new TypeScriptFileGenerator();
       break;
-    case 'csharp':
+    case Languages.csharp:
       if (namespace === undefined) {
         throw new Error('In order to generate types to C#, we need to know which namespace they are under. Add `--namespace=NAMESPACE` to set the desired namespace.');
       }
@@ -45,7 +64,7 @@ export default class Types extends Command {
         namespace
       };
       break;
-    case 'golang':
+    case Languages.golang:
       if (packageName === undefined) {
         throw new Error('In order to generate types to Go, we need to know which package they are under. Add `--packageName=PACKAGENAME` to set the desired package name.');
       }
@@ -54,7 +73,7 @@ export default class Types extends Command {
         packageName
       };
       break;
-    case 'java':
+    case Languages.java:
       if (packageName === undefined) {
         throw new Error('In order to generate types to Java, we need to know which package they are under. Add `--packageName=PACKAGENAME` to set the desired package name.');
       }
@@ -63,24 +82,36 @@ export default class Types extends Command {
         packageName
       };
       break;
-    case 'javascript':
+    case Languages.javascript:
       fileGenerator = new JavaScriptFileGenerator();
+      break;
+    case Languages.dart:
+      fileGenerator = new DartFileGenerator();
       break;
     default:
       throw new Error(`Could not determine generator for language ${language}`);
     }
-    Logger.setLogger({
-      info: this.log,
-      debug: this.debug,
-      warn: this.warn,
-      error: this.error,
-    });
-    const models = await fileGenerator.generateToFiles(
-      parsedInput as any, 
-      output, 
-      {...fileOptions, } as any);
-    const generatedModelNames = models.map((model) => {return model.modelName;});
-
-    this.log(`Successfully generated the following models ${generatedModelNames.join(', ')}`);
+    let models;
+    if (output) {
+      models = await fileGenerator.generateToFiles(
+        parsedInput as any,
+        output,
+        { ...fileOptions, } as any);
+      const generatedModels = models.map((model) => { return model.modelName; });
+  
+      this.log(`Successfully generated the following models: ${generatedModels.join(', ')}`);
+    } else {
+      models = await fileGenerator.generateCompleteModels(
+        parsedInput as any,
+        { ...fileOptions } as any);
+      const generatedModels = models.map((model) => { 
+        return `
+## Model name: ${model.modelName}
+${model.result}
+  `;
+      });
+  
+      this.log(`Successfully generated the following models: ${generatedModels.join('\n')}`);
+    }
   }
 }
--- test/commands/generate/types.test.ts
+++ test/commands/generate/types.test.ts
@@ -17,6 +17,44 @@ describe('types', () => {
       done();
     });
 
+  test
+    .stderr()
+    .stdout()
+    .command([...generalOptions, 'typescript', './test/specification.yml'])
+    .it('works when generating in memory', (ctx, done) => {
+      console.log(ctx.stdout);
+      expect(ctx.stderr).to.equals('');
+      expect(ctx.stdout).to.equals(`Successfully generated the following models: 
+## Model name: AnonymousSchema_1
+
+
+class AnonymousSchema_1 {
+  private _displayName?: string;
+  private _email?: string;
+  private _additionalProperties?: Map<String, object | string | number | Array<unknown> | boolean | null>;
+
+  constructor(input: {
+    displayName?: string,
+    email?: string,
+  }) {
+    this._displayName = input.displayName;
+    this._email = input.email;
+  }
+
+  get displayName(): string | undefined { return this._displayName; }
+  set displayName(displayName: string | undefined) { this._displayName = displayName; }
+
+  get email(): string | undefined { return this._email; }
+  set email(email: string | undefined) { this._email = email; }
+
+  get additionalProperties(): Map<String, object | string | number | Array<unknown> | boolean | null> | undefined { return this._additionalProperties; }
+  set additionalProperties(additionalProperties: Map<String, object | string | number | Array<unknown> | boolean | null> | undefined) { this._additionalProperties = additionalProperties; }
+}
+export default AnonymousSchema_1;
+`);
+      done();
+    });
+    
   describe('for TypeScript', () => {  
     test
       .stderr()
@@ -107,6 +145,29 @@ describe('types', () => {
       .stderr()
       .stdout()
       .command([...generalOptions, 'golang', './test/specification.yml', `-o=${ path.resolve(outputDir, './go')}`])
+      .it('fails when no package defined', (ctx, done) => {
+        expect(ctx.stderr).to.equals('Error: In order to generate types to Go, we need to know which package they are under. Add `--packageName=PACKAGENAME` to set the desired package name.\n');
+        expect(ctx.stdout).to.equals('');
+        done();
+      });
+  });
+
+  describe('for Dart', () => {  
+    test
+      .stderr()
+      .stdout()
+      .command([...generalOptions, 'dart', './test/specification.yml', `-o=${ path.resolve(outputDir, './go')}`, '--packageName', 'test.package'])
+      .it('works when file path is passed', (ctx, done) => {
+        expect(ctx.stderr).to.equals('');
+        expect(ctx.stdout).to.equals(
+          'Successfully generated the following models AnonymousSchema1\n'
+        );
+        done();
+      });
+    test
+      .stderr()
+      .stdout()
+      .command([...generalOptions, 'dart', './test/specification.yml', `-o=${ path.resolve(outputDir, './go')}`])
       .it('fails when no package defined', (ctx, done) => {
         expect(ctx.stderr).to.equals('Error: In order to generate types to Go, we need to know which package they are under. Add `--packageName=PACKAGENAME` to set the desired package name.\n');
         expect(ctx.stdout).to.equals('');
