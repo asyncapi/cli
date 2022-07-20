93ee74b2a52f4ab0913bf204c85b98921b728332
souvik <souvikde.ns@gmail.com>
2022-07-20 16:48:00 +0530

    feat: add tests

--- test/commands/generate/template.test.ts
+++ test/commands/generate/template.test.ts
@@ -1,20 +1,68 @@
-import {expect, test} from '@oclif/test';
+import { expect, test } from '@oclif/test';
 // eslint-disable-next-line
 // @ts-ignore
 import rimraf from 'rimraf';
 
+const generalOptions = ['generate:template']
+
+const cleanup = async (filepath: string) => {
+  rimraf.sync(filepath);
+}
+
 describe('template', () => {
   test
     .stdout()
-    .command(['generate:template', 
-      './test/specification.yml', 
-      '@asyncapi/html-template', 
+    .command([
+      ...generalOptions,
+      './test/specification.yml',
+      '@asyncapi/html-template',
       '--output=./test/docs',
       '--force-write'
     ])
     .it('should generate html tempalte', (ctx, done) => {
       expect(ctx.stdout).to.contain('Check out your shiny new generated files at ./test/docs.\n\n');
-      rimraf.sync('./test/docs');
+      cleanup('./test/docs');
       done();
     });
+
+  test
+    .stderr()
+    .command([
+      ...generalOptions,
+      './test/specification.yml',
+      '@asyncapi/html-template',
+      '--output=./test/doc'
+    ])
+    .it('should throw error if output folder is in a git repository', (ctx, done) => {
+      expect(ctx.stderr).to.contain('Error: "./test/doc" is in a git repository with unstaged changes.')
+      cleanup('./test/doc');
+      done();
+    })
+
+    test
+      .stderr()
+      .command([
+        ...generalOptions,
+        './test/specification.yml',
+        '@asyncapi/html-template',
+      ])
+      .it('should throw error if --output flag is not specified', (ctx, done) => {
+        expect(ctx.stderr).to.contain('Error: Missing required flag:')
+        done();
+      })
+
+      test
+        .stdout()
+        .command([
+          ...generalOptions,
+          './test/specification.yml',
+          './test/minimaltemplate',
+          "-p=customParam='Hello From Custom Param'",
+          '--output=./test/docs'
+        ])
+        .it('shoudld pass custom param in the template', (ctx, done) => {
+          expect(ctx.stdout).to.contain('Check out your shiny new generated files at ./test/docs.\n\n');
+          cleanup('./test/docs');
+          done();
+        })
 });
---
+++ test/minimaltemplate/template/index.js
@@ -0,0 +1,11 @@
+import { File, Text } from "@asyncapi/generator-react-sdk";
+
+export default function({ asyncapi, params, originalAsyncAPI }) {
+  return (
+    <File name="asyncapi.md">
+      <Text>This is a markdown file for my application.</Text>
+      <Text>App name is: **{ asyncapi.info().title() }**</Text>
+      <Text>{params.customParam}</Text>
+    </File>
+  );
+}
