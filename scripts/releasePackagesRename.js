5eb38e023d1cc4d5f9531f5c09a5790a5c82d158
Aayush Kumar Sahu <aayushmau5@gmail.com>
2022-09-02 04:12:57 +0530

    add tar.gz packaging setup

--- scripts/releasePackagesRename.js
+++ scripts/releasePackagesRename.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const { rename, access } = require('fs').promises;
+const { rename, access, mkdir } = require('fs').promises;
 const packageJson = require('../package.json');
 const path = require('path');
 const simpleGit = require('simple-git');
@@ -15,15 +15,35 @@ async function fileExists(checkPath) {
   }
 }
 
+async function checkAndRenameFile(generatedPath, newPath) {
+  if (await fileExists(generatedPath)) { 
+    await rename(generatedPath, newPath);
+  }
+}
+
+async function createDirectory(directoryPath) {
+  await mkdir(directoryPath);
+}
+
 async function renameDeb({version, name, sha}) {
   const dist = 'dist/deb';
 
   // deb package naming convention: https://github.com/oclif/oclif/blob/fb5da961f925fa0eba5c5b05c8cee0c9bd156c00/src/upload-util.ts#L51
   const generatedPath = path.resolve(dist, `${name}_${version}.${sha}-1_amd64.deb`);
   const newPath = path.resolve(dist, 'asyncapi.deb');
-  if (await fileExists(generatedPath)) { 
-    await rename(generatedPath, newPath);
-  }
+  await checkAndRenameFile(generatedPath, newPath);
+}
+
+async function renameTar({version, name, sha}) {
+  const dist = 'dist';
+
+  const generatedPath = path.resolve(dist, `${name}-v${version}-${sha}-linux-x64.tar.gz`);
+  // for tarballs, the files are generated in `dist/` directory.
+  // Creates a new `tar` directory(`dist/tar`), and moves the generated tarball inside that directory.
+  const tarDirectory = path.resolve(dist, 'tar');
+  await createDirectory(tarDirectory);
+  const newPath = path.resolve(tarDirectory, 'asyncapi.tar.gz');
+  await checkAndRenameFile(generatedPath, newPath);
 }
 
 async function renamePkg({version, name, sha}) {
@@ -31,9 +51,7 @@ async function renamePkg({version, name, sha}) {
 
   const generatedPath = path.resolve(dist, `${name}-v${version}-${sha}.pkg`);
   const newPath = path.resolve(dist, 'asyncapi.pkg');
-  if (await fileExists(generatedPath)) { 
-    await rename(generatedPath, newPath);
-  }
+  await checkAndRenameFile(generatedPath, newPath);
 }
 
 async function renamePackages() {
@@ -42,6 +60,7 @@ async function renamePackages() {
   const sha = await git.revparse(['--short', 'HEAD']);
   await renameDeb({version: version.split('-')[0], name, sha});
   await renamePkg({version, name, sha});
+  await renameTar({version, name, sha});
 }
 
 renamePackages();
