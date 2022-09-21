435461f8e236947364f1c3fab5f817699f648781
Matatjahu <urbanczyk.maciej.95@gmail.com>
2022-09-21 15:25:36 +0200

    update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -7,20 +7,8 @@ const config: Config.InitialOptions = {
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
   },
-  // The root of your source code, typically /src
-  // `<rootDir>` is a token Jest substitutes
   roots: ['<rootDir>'],
-  // moduleNameMapper: {
-  //   '^nimma/legacy$': '<rootDir>/node_modules/nimma/dist/legacy/cjs/index.js',
-  //   '^nimma/(.*)': '<rootDir>/node_modules/nimma/dist/cjs/$1',
-  //   '^@stoplight/spectral-ruleset-bundler/(.*)$': '<rootDir>/node_modules/@stoplight/spectral-ruleset-bundler/dist/$1'
-  // },
-  
-  // Test spec file resolution pattern
-  // Matches parent folder `__tests__` and filename
-  // should contain `test` or `spec`.
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
-  // Module file extensions for importing
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testTimeout: 10000,
   setupFiles: ['./test/jest.setup.ts'],
