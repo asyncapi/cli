adfe0534bd352a289ea9de42eb8ff023e8bf273e
Pedro Ramos <81691177+peter-rr@users.noreply.github.com>
2022-09-27 16:17:15 +0200

    fix: wrong exit code for validation error (#288)
    Co-authored-by: derberg <lpgornicki@gmail.com>

--- src/base.ts
+++ src/base.ts
@@ -3,5 +3,6 @@ import {Command} from '@oclif/core';
 export default abstract class extends Command {
   async catch(e: any) {
     console.error(`${e.name}: ${e.message}`);
+    process.exitCode = 1;
   }
 }
