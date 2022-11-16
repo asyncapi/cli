9a6881df566d777f6f2c887e41ee54cd449e37f9
souvik <souvikde.ns@gmail.com>
2022-11-16 20:17:13 +0530

    refactor: lint fixes

--- src/commands/bundle.ts
+++ src/commands/bundle.ts
@@ -1,85 +1,80 @@
-import { Flags } from "@oclif/core";
-import { Example } from "@oclif/core/lib/interfaces";
-import Command from "../base";
-import bundle from "@asyncapi/bundler";
-import fs from "fs";
-import path from "path";
-import { ErrorLoadingSpec } from "../errors/specification-file";
+import { Flags } from '@oclif/core';
+import { Example } from '@oclif/core/lib/interfaces';
+import Command from '../base';
+import bundle from '@asyncapi/bundler';
+import fs from 'fs';
+import path from 'path';
+import { ErrorLoadingSpec } from '../errors/specification-file';
 
 export default class Bundle extends Command {
-  static description = "Bundle multiple asyncapi files together.";
+  static description = 'Bundle multiple asyncapi files together.';
   static strict = false;
 
   static examples: Example[] = [
-    "asyncapi bundle ./asyncapi.yaml -o final.yaml",
+    'asyncapi bundle ./asyncapi.yaml -o final.yaml',
   ];
 
   static flags = {
-    help: Flags.help({ char: "h" }),
-    output: Flags.string({ char: "o" }),
-    referenceIntoComponents: Flags.boolean({ char: "r" }),
-    base: Flags.string({ char: "b" }),
+    help: Flags.help({ char: 'h' }),
+    output: Flags.string({ char: 'o' }),
+    referenceIntoComponents: Flags.boolean({ char: 'r' }),
+    base: Flags.string({ char: 'b' }),
   };
 
   async run() {
     const { argv, flags } = await this.parse(Bundle);
-    const output = flags.output || "main.yml";
+    const output = flags.output || 'main.yml';
 
-    try {
-      this.resolveFilePaths(argv, flags);
-    } catch (error) {
-      throw error;
-    }
+    this.resolveFilePaths(argv, flags);
 
     const document = await bundle(
       argv.map((filePath) =>
-        fs.readFileSync(path.resolve(process.cwd(), filePath), "utf-8")
+        fs.readFileSync(path.resolve(process.cwd(), filePath), 'utf-8')
       ),
       {
-        referenceIntoComponents: flags["referenceIntoComponents"],
+        referenceIntoComponents: flags['referenceIntoComponents'],
         base: flags.base
           ? fs.readFileSync(
-              path.resolve(process.cwd(), flags.base || ""),
-              "utf-8"
-            )
+            path.resolve(process.cwd(), flags.base || ''),
+            'utf-8'
+          )
           : undefined,
       }
     );
 
     const format = path.extname(output);
 
-    if (format === ".yml" || format === ".yaml") {
+    if (format === '.yml' || format === '.yaml') {
       fs.writeFileSync(path.resolve(process.cwd(), output), document.yml(), {
-        encoding: "utf-8",
+        encoding: 'utf-8',
       });
     }
 
-    if (format === ".json") {
+    if (format === '.json') {
       fs.writeFileSync(path.resolve(process.cwd(), output), document.json(), {
-        encoding: "utf-8",
+        encoding: 'utf-8',
       });
     }
-    this.log(
-      `Check out your shiny new bundled files at ${output}`
-    );
+    this.log(`Check out your shiny new bundled files at ${output}`);
   }
 
   private checkFilePath(filePath: string) {
     try {
-      var stats = fs.statSync(path.resolve(process.cwd(), filePath));
+      const stats = fs.statSync(path.resolve(process.cwd(), filePath));
+      return fs.existsSync(filePath) && stats.isFile();
     } catch (error) {
       return false;
     }
-    return fs.existsSync(filePath) && stats.isFile();
   }
 
   private resolveFilePaths(arg: any, flags: any) {
     for (const file of arg) {
-      if (!this.checkFilePath(file)) throw new ErrorLoadingSpec("file", file);
+      if (!this.checkFilePath(file)) {
+        throw new ErrorLoadingSpec('file', file);
+      }
     }
-    if (flags.base) {
-      if (!this.checkFilePath(flags.base))
-        throw new ErrorLoadingSpec("file", flags.base);
+    if (flags.base && !this.checkFilePath(flags.base)) {
+      throw new ErrorLoadingSpec('file', flags.base);
     }
   }
 }
--- test/commands/bundle/bundle.test.ts
+++ test/commands/bundle/bundle.test.ts
@@ -1,22 +1,22 @@
-import { test } from "@oclif/test";
+import { test } from '@oclif/test';
 import fs from 'fs';
 
-function cleanup(filepath: string){
-    fs.unlinkSync(filepath);
+function cleanup(filepath: string) {
+  fs.unlinkSync(filepath);
 }
 
-describe("bundle", () => {
+describe('bundle', () => {
   test
     .stdout()
     .command([
-      "bundle","./test/commands/bundle/asyncapi.yaml",
-      "--output=./test/commands/bundle/final.yaml",
+      'bundle','./test/commands/bundle/asyncapi.yaml',
+      '--output=./test/commands/bundle/final.yaml',
     ])
-    .it("should successfully bundle specification", (ctx, done) => {
+    .it('should successfully bundle specification', (ctx, done) => {
       expect(ctx.stdout).toContain(
-        "Check out your shiny new bundled files at ./test/commands/bundle/final.yaml"
+        'Check out your shiny new bundled files at ./test/commands/bundle/final.yaml'
       );
-      cleanup('./test/commands/bundle/final.yaml')
+      cleanup('./test/commands/bundle/final.yaml');
       done();
     });
 });
