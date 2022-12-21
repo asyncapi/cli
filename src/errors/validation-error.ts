8773458c8e3a8e1cde5b1d697218fc001ad1eb8d
derberg <lpgornicki@gmail.com>
2022-12-21 10:32:14 +0100

    revert type change

--- src/errors/validation-error.ts
+++ src/errors/validation-error.ts
@@ -22,7 +22,7 @@ export class ValidationError extends Error {
   }
 
   /* eslint-disable sonarjs/cognitive-complexity */
-  private buildError(err: any, filePath: string) { // NOSONAR
+  private buildError(err: any, filePath: any) { // NOSONAR
     const errorsInfo: Array<string> = [];
 
     if (filePath) {
