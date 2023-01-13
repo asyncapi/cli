c030e5ff1f8b57e35e452569c79ed46089869716
Khuda Dad Nomani <khudadad414@gmail.com>
2023-01-13 10:44:25 +0000

    refactor fromTemplate tests

--- test/commands/generate/fromTemplate.test.ts
+++ test/commands/generate/fromTemplate.test.ts
@@ -13,7 +13,7 @@ async function cleanup(filepath: string) {
   rimraf.sync(filepath);
 }
 
-describe('git-test', () => {
+describe('template-git', () => {
   test
     .stderr()
     .command([...generalOptions, '--output=./test/doc'])
@@ -29,7 +29,7 @@ describe('git-test', () => {
     );
 });
 
-describe('template', () => {
+describe('template-minimal', () => {
   test
     .stdout()
     .command([...generalOptions, '--output=./test/docs', '--force-write'])
@@ -40,7 +40,9 @@ describe('template', () => {
       cleanup('./test/docs');
       done();
     });
+});
 
+describe('template-custom', () => {
   test
     .stdout()
     .command([
@@ -56,102 +58,102 @@ describe('template', () => {
       cleanup('./test/docs');
       done();
     });
+});
 
-  describe('disable-hooks', () => {
-    test
-      .stdout()
-      .command([
-        ...generalOptions,
-        '--output=./test/docs',
-        '--force-write',
-        '-d=generate:after',
-      ])
-      .it('should not create asyncapi.yaml file', (_, done) => {
-        const exits = fs.existsSync(path.resolve('./docs/asyncapi.yaml'));
-        expect(exits).toBeFalsy();
-        cleanup('./test/docs');
-        done();
-      });
-  });
+describe('disable-hooks', () => {
+  test
+    .stdout()
+    .command([
+      ...generalOptions,
+      '--output=./test/docs',
+      '--force-write',
+      '-d=generate:after',
+    ])
+    .it('should not create asyncapi.yaml file', (_, done) => {
+      const exits = fs.existsSync(path.resolve('./docs/asyncapi.yaml'));
+      expect(exits).toBeFalsy();
+      cleanup('./test/docs');
+      done();
+    });
+});
 
-  describe('debug', () => {
-    test
-      .stdout()
-      .command([
-        ...generalOptions,
-        '--output=./test/docs',
-        '--force-write',
-        '--debug',
-      ])
-      .it('should print debug logs', (ctx, done) => {
-        expect(ctx.stdout).toContain(
-          `Template sources taken from ${path.resolve(
-            './test/minimaltemplate'
-          )}.`
-        );
-        cleanup('./test/docs');
-        done();
-      });
-  });
+describe('debug', () => {
+  test
+    .stdout()
+    .command([
+      ...generalOptions,
+      '--output=./test/docs',
+      '--force-write',
+      '--debug',
+    ])
+    .it('should print debug logs', (ctx, done) => {
+      expect(ctx.stdout).toContain(
+        `Template sources taken from ${path.resolve(
+          './test/minimaltemplate'
+        )}.`
+      );
+      cleanup('./test/docs');
+      done();
+    });
+});
 
-  describe('no-overwrite', () => {
-    test
-      .stdout()
-      .command([
-        ...generalOptions,
-        '--output=./test/docs',
-        '--force-write',
-        '--no-overwrite=./test/docs/asyncapi.md',
-      ])
-      .it('should skip the filepath and generate normally', (ctx, done) => {
+describe('no-overwrite', () => {
+  test
+    .stdout()
+    .command([
+      ...generalOptions,
+      '--output=./test/docs',
+      '--force-write',
+      '--no-overwrite=./test/docs/asyncapi.md',
+    ])
+    .it('should skip the filepath and generate normally', (ctx, done) => {
+      expect(ctx.stdout).toContain(
+        'Check out your shiny new generated files at ./test/docs.\n\n'
+      );
+      cleanup('./test/docs');
+      done();
+    });
+});
+
+describe('install', () => {
+  jest.setTimeout(100000);
+
+  test
+    .stdout()
+    .command([
+      'generate:fromTemplate',
+      './test/specification.yml',
+      './test/minimaltemplate',
+      '--install',
+      '--force-write',
+      '--output=./test/docs'
+    ])
+    .it('should install template', (ctx, done) => {
+      expect(ctx.stdout).toContain('Template installation started because you passed --install flag.');
+      cleanup('./test/docs');
+      done();
+    });
+});
+
+describe('map-base-url', () => {
+  test
+    .stdout()
+    .command([
+      'generate:fromTemplate',
+      './test/dummyspec/apiwithref.json',
+      '@asyncapi/minimaltemplate',
+      '--output=./test/docs',
+      '--force-write',
+      '--map-base-url=https://schema.example.com/crm/:./test/dummyspec',
+    ])
+    .it(
+      'should resolve reference and generate from template',
+      (ctx, done) => {
         expect(ctx.stdout).toContain(
           'Check out your shiny new generated files at ./test/docs.\n\n'
         );
         cleanup('./test/docs');
         done();
-      });
-  });
-
-  describe('install', () => {
-    jest.setTimeout(100000);
-
-    test
-      .stdout()
-      .command([
-        'generate:fromTemplate',
-        './test/specification.yml',
-        './test/minimaltemplate',
-        '--install',
-        '--force-write',
-        '--output=./test/docs'
-      ])
-      .it('should install template', (ctx, done) => {
-        expect(ctx.stdout).toContain('Template installation started because you passed --install flag.');
-        cleanup('./test/docs');
-        done();
-      });
-  });
-
-  describe('map-base-url', () => {
-    test
-      .stdout()
-      .command([
-        'generate:fromTemplate',
-        './test/dummyspec/apiwithref.json',
-        '@asyncapi/minimaltemplate',
-        '--output=./test/docs',
-        '--force-write',
-        '--map-base-url=https://schema.example.com/crm/:./test/dummyspec',
-      ])
-      .it(
-        'should resolve reference and generate from template',
-        (ctx, done) => {
-          expect(ctx.stdout).toContain(
-            'Check out your shiny new generated files at ./test/docs.\n\n'
-          );
-          cleanup('./test/docs');
-          done();
-        }
-      );
-  });
+      }
+    );
 });
