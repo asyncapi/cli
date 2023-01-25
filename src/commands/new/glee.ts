6c5c10cad3f9503bfd8fac1b09c31312706f56dd
Pedro Ramos <pedroramosromero@gmail.com>
2023-01-25 20:35:22 +0100

    Make use of 'copy' function from 'fs-extra' library

--- src/commands/new/glee.ts
+++ src/commands/new/glee.ts
@@ -3,9 +3,10 @@ import { fstat, promises as fPromises } from 'fs';
 import Command from '../../base';
 import { resolve } from 'path';
 import path from 'path';
-import { fileURLToPath } from 'url';
+import fs from 'fs-extra';
 
 const { writeFile, readFile } = fPromises;
+const fs = require('fs-extra');
 
 export default class NewGlee extends Command {
   static description = 'Creates a new Glee project';
@@ -20,27 +21,22 @@ export default class NewGlee extends Command {
 
     const projectName = flags.name;
 
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    console.log(__dirname);
+    const PROJECT_DIRECTORY = path.join(process.cwd(), projectName); // cwd-> /Users/pedro.ramos/cli
+    const GLEE_TEMPLATES_DIRECTORY = resolve(__dirname, '../../../create-glee-app/templates/default'); // __dirname-> /Users/pedro.ramos/cli/lib/commands/new
 
-    const PROJECT_DIRECTORY = path.resolve(__dirname, `../../../${projectName}`);
-    const GLEE_TEMPLATES_DIRECTORY = path.resolve(__dirname, '../../../create-glee-app/templates/default');
+    try {
+      await fPromises.mkdir(PROJECT_DIRECTORY);
+      console.log('Glee project folder created!');
+    } catch (err) {
+      console.error('An existing folder is already using the same name of your Glee project. Please specify a different name for the new project, or just remove the existing folder from the current directory.');
+      return;
+    }
     
-    // Create glee project folder
-    await fPromises.mkdir(PROJECT_DIRECTORY);
-    console.log('Glee project folder created!');
-
-    // Add 'functions' folder to Glee project
-    await fPromises.copyFile(path.join(GLEE_TEMPLATES_DIRECTORY, 'functions'), PROJECT_DIRECTORY);
-    console.log('functions folder added.');
-
-    // Add 'asyncapi.yaml' file to Glee project
-    await fPromises.copyFile(path.join(GLEE_TEMPLATES_DIRECTORY, 'asyncapi.yaml'), PROJECT_DIRECTORY);
-    console.log('asyncapi.yaml file added.');
-
-    // Add 'package.json' file to Glee project
-    await fPromises.copyFile(path.join(GLEE_TEMPLATES_DIRECTORY, 'package.json'), PROJECT_DIRECTORY);
-    console.log('package.json file added.');
+    try {
+      await fs.copy(GLEE_TEMPLATES_DIRECTORY, PROJECT_DIRECTORY);
+      console.log('Content copied to your project folder!');
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
