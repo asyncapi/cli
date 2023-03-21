3ee56871ff614d134e79796432d590927d3942f7
Abhay Garg <abhay.garg@cred.club>
2023-03-21 17:49:45 +0530

    add csharpAutoImplement flag

--- src/commands/generate/models.ts
+++ src/commands/generate/models.ts
@@ -1,4 +1,4 @@
-import { CSharpFileGenerator, JavaFileGenerator, JavaScriptFileGenerator, TypeScriptFileGenerator, GoFileGenerator, Logger, DartFileGenerator, PythonFileGenerator, RustFileGenerator } from '@asyncapi/modelina';
+import { CSharpFileGenerator, JavaFileGenerator, JavaScriptFileGenerator, TypeScriptFileGenerator, GoFileGenerator, Logger, DartFileGenerator, PythonFileGenerator, RustFileGenerator, CSHARP_DEFAULT_PRESET } from '@asyncapi/modelina';
 import { Flags } from '@oclif/core';
 import Command from '../../base';
 import { load } from '../../models/SpecificationFile';
@@ -82,12 +82,17 @@ export default class Models extends Command {
       description: 'C# specific, define the namespace to use for the generated models. This is required when language is `csharp`.',
       required: false
     }),
+    csharpAutoImplement: Flags.boolean({
+      description: 'C# specific, define whether to generate auto-implemented properties or not. This is required when language is `csharp`.',
+      required: false,
+      default: false
+    }),
     ...validationFlags({ logDiagnostics: false }),
   };
 
   async run() {
     const { args, flags } = await this.parse(Models);
-    const { tsModelType, tsEnumType, tsModuleSystem, tsExportType, namespace, packageName, output } = flags;
+    const { tsModelType, tsEnumType, tsModuleSystem, tsExportType, namespace, csharpAutoImplement, packageName, output } = flags;
     const { language, file } = args;
 
     const inputFile = (await load(file)) || (await load());
@@ -134,7 +139,16 @@ export default class Models extends Command {
       if (namespace === undefined) {
         throw new Error('In order to generate models to C#, we need to know which namespace they are under. Add `--namespace=NAMESPACE` to set the desired namespace.');
       }
-      fileGenerator = new CSharpFileGenerator();
+      fileGenerator = new CSharpFileGenerator({
+        presets: csharpAutoImplement ? [
+          {
+            preset: CSHARP_DEFAULT_PRESET,
+            options: {
+              autoImplementedProperties: true
+            }
+          }
+        ] : []
+      });
       fileOptions = {
         namespace
       };
--- test/commands/generate/models.test.ts
+++ test/commands/generate/models.test.ts
@@ -113,6 +113,17 @@ describe('models', () => {
         expect(ctx.stdout).toEqual('');
         done();
       });
+    test
+      .stderr()
+      .stdout()
+      .command([...generalOptions, 'csharp', './test/specification.yml', `-o=${ path.resolve(outputDir, './csharp')}`, '--namespace=\'asyncapi.models\''])
+      .it('works when auto implement properties flag is passed', (ctx, done) => {
+        expect(ctx.stderr).toEqual('');
+        expect(ctx.stdout).toContain(
+          'Successfully generated the following models: '
+        );
+        done();
+      });
   });
 
   describe('for Java', () => {
