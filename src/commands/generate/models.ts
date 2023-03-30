cc10350e25f13d97908c61b5d9d3eef2b95567e9
Abhay Garg <54636525+prayutsu@users.noreply.github.com>
2023-03-30 19:27:09 +0530

    feat: generate jsonbinpack support for TypeScript models (#483)
    Co-authored-by: Abhay Garg <abhay.garg@cred.club>%0ACo-authored-by: Lukasz Gornicki <lpgornicki@gmail.com>

--- src/commands/generate/models.ts
+++ src/commands/generate/models.ts
@@ -1,4 +1,4 @@
-import { CSharpFileGenerator, JavaFileGenerator, JavaScriptFileGenerator, TypeScriptFileGenerator, GoFileGenerator, Logger, DartFileGenerator, PythonFileGenerator, RustFileGenerator, CSHARP_DEFAULT_PRESET } from '@asyncapi/modelina';
+import { CSharpFileGenerator, JavaFileGenerator, JavaScriptFileGenerator, TypeScriptFileGenerator, GoFileGenerator, Logger, DartFileGenerator, PythonFileGenerator, RustFileGenerator, TS_COMMON_PRESET, TS_JSONBINPACK_PRESET, CSHARP_DEFAULT_PRESET } from '@asyncapi/modelina';
 import { Flags } from '@oclif/core';
 import Command from '../../base';
 import { load } from '../../models/SpecificationFile';
@@ -68,6 +68,11 @@ export default class Models extends Command {
       required: false,
       default: 'default',
     }),
+    tsJsonBinPack: Flags.boolean({
+      description: 'TypeScript specific, define basic support for serializing to and from binary with jsonbinpack.',
+      required: false,
+      default: false,
+    }),
     /**
      * Go and Java specific package name to use for the generated models
      */
@@ -96,10 +101,11 @@ export default class Models extends Command {
     }),
     ...validationFlags({ logDiagnostics: false }),
   };
-
+  
+  /* eslint-disable sonarjs/cognitive-complexity */
   async run() {
     const { args, flags } = await this.parse(Models);
-    const { tsModelType, tsEnumType, tsModuleSystem, tsExportType, namespace, csharpAutoImplement, csharpArrayType, packageName, output } = flags;
+    const { tsModelType, tsEnumType, tsModuleSystem, tsExportType, tsJsonBinPack, namespace, csharpAutoImplement, csharpArrayType, packageName, output } = flags;
     const { language, file } = args;
 
     const inputFile = (await load(file)) || (await load());
@@ -130,6 +136,15 @@ export default class Models extends Command {
       fileGenerator = new TypeScriptFileGenerator({
         modelType: tsModelType as 'class' | 'interface',
         enumType: tsEnumType as 'enum' | 'union',
+        presets: tsJsonBinPack ? [
+          {
+            preset: TS_COMMON_PRESET,
+            options: {
+              marshalling: true
+            }
+          },
+          TS_JSONBINPACK_PRESET
+        ] : []
       });
       fileOptions = {
         moduleSystem: tsModuleSystem,
