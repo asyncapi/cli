2ac64e7e1804657f1a555418516fccd78befa2c9
Viacheslav Turovskyi <aeworxet@protonmail.com>
2023-04-28 03:21:32 +0000

    feat: add `versions` to output versions of AsyncAPI tools used

---
+++ src/commands/config/versions.ts
@@ -0,0 +1,51 @@
+import { Flags } from '@oclif/core';
+import Command from '../../base';
+
+export default class Versions extends Command {
+  static description = 'Show versions of AsyncAPI tools used';
+  
+  static flags = {
+    help: Flags.help({ char: 'h' }),
+  };
+
+  private dependencies: string[] = [];
+
+  async run() {
+    // Preparation of the array with all dependencies '@asyncapi/*' along with
+    // their versions.
+    for (let key in this.config.pjson.dependencies) {
+      // Making sure with .indexOf() that only package names which START with
+      // string '@asyncapi' are considered.
+      if (key.indexOf('@asyncapi', 0) === 0) {
+
+        // Avoiding obvious crash on manual removal or alteration of an
+        // '@asyncapi' package.
+        try {
+          // Goofy name `importedPJSON` is chosen to distinguish from name `pjson`
+          // used in `@oclif` source code.
+          const importedPJSON = await import(key + '/package.json');
+          key = key + '/' + importedPJSON.default.version;
+        } catch (e) {
+          key = key + '/' + '`package.json` not found';
+        }
+
+        this.dependencies.push(key);
+      }
+    }
+
+    // Showing information available with `--version` flag.
+    this.log(this.config.userAgent);
+
+    // Iteration through the array containing all dependencies '@asyncapi/*'
+    // along with their versions.
+    for (let i = 0; i < this.dependencies.length; i++) {
+      if (i !== this.dependencies.length - 1) {
+        this.log('  ├' + this.dependencies[i]);
+      } else {
+        this.log('  └' + this.dependencies[i] + '\n');
+      }
+    }
+
+    this.log('Repository: ' + this.config.pjson.homepage);
+  }
+}
