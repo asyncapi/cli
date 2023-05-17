5547e3a54590d58d67476ce6436f254e8c4b63c2
Mahfuza Humayra Mohona <mhmohona@gmail.com>
2023-05-17 21:35:50 +0600

    updating script

--- scripts/update_usage_docs.js
+++ scripts/update_usage_docs.js
@@ -1,9 +1,11 @@
-import fs from 'fs/promises';
-import { exec } from 'child_process/promises';
+const fs = require('fs').promises;
+const { exec } = require('child_process');
 
 // Define the paths to the README (which will be created inside the ./script folder) and usage files
-const README_PATH = './README.md';
-const USAGE_PATH = '../docs/usage.md';
+const README_PATH = './scripts/README.md';
+const USAGE_PATH = './docs/usage.md';
+// const README_PATH = './README.md';
+// const USAGE_PATH = '../docs/usage.md';
 
 // Append the usage and commands tags to the README file
 // The readme must have any of the following tags inside of it for it to be replaced after running `oclif readme` command
@@ -11,10 +13,19 @@ const USAGE_PATH = '../docs/usage.md';
 // <!-- usage -->
 // # Commands
 // <!-- commands -->
-fs.appendFile(README_PATH, '\n\n# Usage\n\n<!-- usage -->\n\n# Commands\n\n<!-- commands -->\n');
-
-// Generate the usage documentation using the `oclif readme` command
-exec('oclif readme')
+fs.appendFile(README_PATH, '\n\n# Usage\n\n<!-- usage -->\n\n# Commands\n\n<!-- commands -->\n')
+  .then(() => {
+    // Generate the usage documentation using the `oclif readme` command
+    return new Promise((resolve, reject) => {
+      exec('oclif readme', (error, stdout, stderr) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
+    });
+  })
   .then(() => {
     // Define the header for the usage file
     const header = `---
@@ -25,10 +36,15 @@ weight: 40
 The AsyncAPI CLI makes it easier to work with AsyncAPI documents.
 `;
 
-    fs.writeFile(USAGE_PATH, header);
+    // Write the header to the usage file
+    return fs.writeFile(USAGE_PATH, header);
+  })
+  .then(() => {
+    // Read the contents of the README file
     return fs.readFile(README_PATH, 'utf8');
   })
   .then((readmeContents) => {
+    // Append the README contents to the usage file
     return fs.appendFile(USAGE_PATH, `\n${readmeContents}`);
   })
   .then(() => {
