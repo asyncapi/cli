611069dc534965fbcc844934e500bae55c851a43
Mahfuza Humayra Mohona <mhmohona@gmail.com>
2023-05-17 23:42:46 +0600

    updating to pass test

--- scripts/update_usage_docs.js
+++ scripts/update_usage_docs.js
@@ -1,17 +1,17 @@
-// Import the necessary modules
-const fs = require('fs').promises;
-const { exec } = require('child_process');
+import { promises as fs } from 'fs';
+import { exec } from 'child_process';
 
-// Define the paths to the README and usage files
-const README_PATH = './scripts/README.md'; // File path for the generated README file
-const USAGE_PATH = './docs/usage.md'; // File path for the usage documentation file
+const README_PATH = './scripts/README.md';
+const USAGE_PATH = './docs/usage.md';
 
-// Append usage and commands tags to the README file
-// These tags are later replaced by the `oclif readme` command with actual usage documentation
-fs.appendFile(README_PATH, '\n\n# Usage\n\n<!-- usage -->\n\n# Commands\n\n<!-- commands -->\n')
-  .then(() => {
+(async () => {
+  try {
+    // Append usage and commands tags to the README file
+    // These tags are later replaced by the `oclif readme` command with actual usage documentation
+    await fs.appendFile(README_PATH, '\n\n# Usage\n\n<!-- usage -->\n\n# Commands\n\n<!-- commands -->\n');
+
     // Generate the usage documentation using the `oclif readme` command
-    return new Promise((resolve, reject) => {
+    await new Promise((resolve, reject) => {
       exec('oclif readme', { cwd: './scripts' }, (error, stdout, stderr) => {
         if (error) {
           reject(error);
@@ -20,8 +20,7 @@ fs.appendFile(README_PATH, '\n\n# Usage\n\n<!-- usage -->\n\n# Commands\n\n<!-- 
         }
       });
     });
-  })
-  .then(() => {
+
     // Define the header for the usage file
     const header = `---
 title: 'Usage'
@@ -32,20 +31,17 @@ The AsyncAPI CLI makes it easier to work with AsyncAPI documents.
 `;
 
     // Write the header to the usage file
-    return fs.writeFile(USAGE_PATH, header);
-  })
-  .then(() => {
+    await fs.writeFile(USAGE_PATH, header);
+
     // Read the contents of the README file
-    return fs.readFile(README_PATH, 'utf8');
-  })
-  .then((readmeContents) => {
+    const readmeContents = await fs.readFile(README_PATH, 'utf8');
+
     // Append the README contents to the usage file
-    return fs.appendFile(USAGE_PATH, `\n${readmeContents}`);
-  })
-  .then(() => {
+    await fs.appendFile(USAGE_PATH, `\n${readmeContents}`);
+
     // Remove the generated README file
-    return fs.unlink(README_PATH);
-  })
-  .catch((err) => {
+    await fs.unlink(README_PATH);
+  } catch (err) {
     console.error(err);
-  });
+  }
+})();
