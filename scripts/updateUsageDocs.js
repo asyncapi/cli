6d4ebdac50b4adcdc32532f5b85f6d6d35e41302
Mahfuza Humayra Mohona <mhmohona@gmail.com>
2023-05-18 19:29:53 +0600

    update

--- scripts/updateUsageDocs.js
+++ scripts/updateUsageDocs.js
@@ -1,12 +1,11 @@
-const { writeFile, readFile } = require('fs').promises;
+/* eslint-disable @typescript-eslint/no-var-requires */
+const {writeFile, readFile} = require('fs').promises;
 
 // Define the paths to the README and usage files
 const README_PATH = './scripts/README.md'; // File path for the generated README file
-const USAGE_PATH = './docs/usage.md'; 
+const USAGE_PATH = './docs/usage.md'; // File path for the usage documentation file
 
-// Create a header for the usage.md file
-const header = `
----
+const header = `---
 title: 'Usage'
 weight: 40
 ---
@@ -16,11 +15,14 @@ The AsyncAPI CLI makes it easier to work with AsyncAPI documents.
 
 // Define an async function to write the header and the README contents to the usage documentation file
 async function run() {
-  await writeFile(USAGE_PATH, header, { encoding: 'utf8' });
-  const readmeContents = await readFile(README_PATH, 'utf8');
-
-  // Append the contents of the README file to the usage documentation file
-  await writeFile(USAGE_PATH, readmeContents, { encoding: 'utf8', flag: 'a' });
+  try {
+    await writeFile(USAGE_PATH, header);
+    const readmeContents = await readFile(README_PATH, 'utf8');
+      // Append the contents of the README file to the usage documentation file
+    await writeFile(USAGE_PATH, readmeContents, { flag: 'a' });
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 run();
