73281265f8d4dfc0f04128520ec5c123bfd63aed
chinmayshewale <shewalechinmay54@gmail.com>
2023-05-25 01:11:36 +0530

    Added tsMarshalling and tsExampleInstance flag

--- src/commands/generate/models.ts
+++ src/commands/generate/models.ts
@@ -82,6 +82,16 @@ export default class Models extends Command {
       required: false,
       default: false,
     }),
+    tsExampleInstance: Flags.boolean({
+      description:'Typescript specific, if enabled add example while generating models',
+      required: false,
+      default: false,
+    }),
+    tsMarshalling: Flags.boolean({
+      description:'Typescript specific, if enabled add marshalling functions while generating models',
+      required: false,
+      default: false,
+    }),
     /**
      * Go and Java specific package name to use for the generated models
      */
@@ -139,7 +149,7 @@ export default class Models extends Command {
   /* eslint-disable sonarjs/cognitive-complexity */
   async run() {
     const { args, flags } = await this.parse(Models);
-    const { tsModelType, tsEnumType, tsIncludeComments, tsModuleSystem, tsExportType, tsJsonBinPack, namespace, csharpAutoImplement, csharpArrayType, csharpNewtonsoft, csharpHashcode, csharpEqual, csharpSystemJson, packageName, output } = flags;
+    const { tsModelType, tsEnumType, tsIncludeComments, tsModuleSystem, tsExportType, tsJsonBinPack, tsExampleInstance , tsMarshalling ,namespace, csharpAutoImplement, csharpArrayType, csharpNewtonsoft, csharpHashcode, csharpEqual, csharpSystemJson, packageName, output } = flags;
     const { language, file } = args;
     const inputFile = (await load(file)) || (await load());
     const { document, status } = await parse(this, inputFile, flags);
@@ -165,15 +175,17 @@ export default class Models extends Command {
     let fileGenerator: AbstractGenerator<any, any> & AbstractFileGenerator<any>;
     let fileOptions: any = {};
     const presets = [];
+    const options={
+      example: tsExampleInstance,
+      marshalling: tsMarshalling,
+    }
     switch (language) {
     case Languages.typescript:
+      presets.push({preset: TS_COMMON_PRESET,options:options});
       if (tsIncludeComments) {presets.push(TS_DESCRIPTION_PRESET);}
       if (tsJsonBinPack) {
         presets.push({
           preset: TS_COMMON_PRESET,
-          options: {
-            marshalling: true
-          }
         },
         TS_JSONBINPACK_PRESET);
       }
