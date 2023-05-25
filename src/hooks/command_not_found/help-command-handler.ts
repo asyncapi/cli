6dc7d431907697e4c7ac843bea71cc1035a67aea
Sambhav Gupta <sambhavgupta0705@gmail.com>
2023-05-26 01:52:05 +0530

    fix: fixed the console command

--- src/hooks/command_not_found/help-command-handler.ts
+++ src/hooks/command_not_found/help-command-handler.ts
@@ -1,11 +1,9 @@
 import {Hook} from '@oclif/core';
 
 const helpCommandHandlerHook: Hook<'command_not_found'> = async function (opts) {
-  console.log(opts);
   if (opts.id === 'help') {
-    console.log('help command doesn\'t exist');
+    console.log('help command doesn\'t exist try --help');
   }
-  // may be figure out a way to exit before the plugin "@oclif/plugin-not-found" kicks in
 };
 
 export default helpCommandHandlerHook;
