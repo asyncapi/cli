24f6af9b3a0aeaa32dc3e4b320506acf15eafca7
Viacheslav Turovskyi <aeworxet@protonmail.com>
2023-07-04 23:16:01 +0000

    feat: support context file location in repository

--- src/commands/config/context/current.ts
+++ src/commands/config/context/current.ts
@@ -1,15 +1,42 @@
 import { Flags } from '@oclif/core';
 import Command from '../../../base';
 import { getCurrentContext } from '../../../models/Context';
+import {
+  MissingContextFileError,
+  ContextFileWrongFormatError,
+  ContextNotFoundError,
+} from '../../../errors/context-error';
 
 export default class ContextCurrent extends Command {
-  static description='Shows the current context that is being used';
-  static flags={
-    help: Flags.help({char: 'h'})
+  static description = 'Shows the current context that is being used';
+  static flags = {
+    help: Flags.help({ char: 'h' }),
   };
 
   async run() {
-    const { current, context } = await getCurrentContext();
-    this.log(`${current}: ${context}`);
+    let fileContent;
+
+    try {
+      fileContent = await getCurrentContext();
+    } catch (e) {
+      if (
+        e instanceof MissingContextFileError ||
+        ContextFileWrongFormatError ||
+        ContextNotFoundError ||
+        (fileContent && !fileContent.current) ||
+        !fileContent
+      ) {
+        this.log(
+          'No context is set as current. Run "asyncapi config context" to see all available options.'
+        );
+        return;
+      } else {
+        throw e;
+      }
+    }
+
+    if (fileContent) {
+      this.log(`${fileContent.current}: ${fileContent.context}`);
+    }
   }
 }
--- src/commands/config/context/list.ts
+++ src/commands/config/context/list.ts
@@ -1,6 +1,10 @@
 import { Flags } from '@oclif/core';
 import Command from '../../../base';
 import { loadContextFile, CONTEXT_FILE_PATH } from '../../../models/Context';
+import {
+  MissingContextFileError,
+  ContextFileWrongFormatError,
+} from '../../../errors/context-error';
 
 export default class ContextList extends Command {
   static description = 'List all the stored contexts in the store';
@@ -9,18 +13,33 @@ export default class ContextList extends Command {
   };
 
   async run() {
-    const fileContent = await loadContextFile();
+    let fileContent;
+
+    try {
+      fileContent = await loadContextFile();
+    } catch (e) {
+      if (e instanceof MissingContextFileError || ContextFileWrongFormatError) {
+        this.log(
+          'You have no context configured. Run "asyncapi config context" to see all available options.'
+        );
+      }
+    }
+
     // If context file contains only one empty property `store` then the whole
     // context file is considered empty.
     if (
+      fileContent &&
       Object.keys(fileContent).length === 1 &&
       Object.keys(fileContent.store).length === 0
     ) {
       this.log(`Context file "${CONTEXT_FILE_PATH}" is empty.`);
       return;
     }
-    for (const [contextName, filePath] of Object.entries(fileContent.store)) {
-      this.log(`${contextName}: ${filePath}`);
+
+    if (fileContent) {
+      for (const [contextName, filePath] of Object.entries(fileContent.store)) {
+        this.log(`${contextName}: ${filePath}`);
+      }
     }
   }
 }
