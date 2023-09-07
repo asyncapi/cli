bed52db10c240bda19f82b7757bb7eae9204c379
Heiko Henning <greenrover33@gmail.com>
2023-09-07 11:25:49 +0000

    feat: support for protobuf schemas

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,3 +3,4 @@ declare module '@asyncapi/specs';
 declare module '@asyncapi/openapi-schema-parser';
 declare module '@asyncapi/avro-schema-parser';
 declare module '@asyncapi/raml-dt-schema-parser';
+declare module '@asyncapi/protobuf-schema-parser';
