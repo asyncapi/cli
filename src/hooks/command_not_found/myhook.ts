29b2df3fc83008a9d223e33adff5655a962befe0
ayush-coder-hai <ayush@betalectic.com>
2023-10-14 02:44:20 +0530

    added the hook .

---
+++ src/hooks/command_not_found/myhook.ts
@@ -0,0 +1,8 @@
+import {Hook} from '@oclif/core'
+
+const hook: Hook<'command_not_found'> = async function (opts) {
+  if(opts.id === "help"){ process.stdout.write(`help hook running ${opts.id}\n`))}
+ 
+}
+
+export default hook
