6a53b610d193901922c081ee04a0197b61d1380c
ayush-coder-hai <ayush@betalectic.com>
2023-10-14 02:52:44 +0530

    added the semicolon

--- src/hooks/command_not_found/myhook.ts
+++ src/hooks/command_not_found/myhook.ts
@@ -1,7 +1,7 @@
 import {Hook} from '@oclif/core'
 
 const hook: Hook<'command_not_found'> = async function (opts) {
-  if(opts.id === "help"){ process.stdout.write(`help hook running ${opts.id}\n`)}
+  if(opts.id === "help"){ process.stdout.write(`help hook running ${opts.id}\n`)};
  
 }
 
