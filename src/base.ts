9952df2814791408dc8f3cbf2d7dc65e8a38ef45
Sergio Moya <1083296+smoya@users.noreply.github.com>
2023-10-22 12:33:40 +0200

    feat: add POC for measuring adoption

--- src/base.ts
+++ src/base.ts
@@ -1,6 +1,9 @@
 import { Command } from '@oclif/core';
+import { Recorder, StdOutSink } from '@smoya/asyncapi-adoption-metrics';
 
 export default abstract class extends Command {
+  recorder = new Recorder('asyncapi-adoption', new StdOutSink());
+
   async catch(err: Error & { exitCode?: number; }): Promise<any> {
     try {
       return await super.catch(err);
--- src/commands/validate.ts
+++ src/commands/validate.ts
@@ -5,6 +5,8 @@ import { validate, validationFlags } from '../parser';
 import { load } from '../models/SpecificationFile';
 import { specWatcher } from '../globals';
 import { watchFlag } from '../flags';
+import { MetadataFromDocument } from '@smoya/asyncapi-adoption-metrics';
+import { Parser } from '@asyncapi/parser';
 
 export default class Validate extends Command {
   static description = 'validate asyncapi file';
@@ -19,6 +21,8 @@ export default class Validate extends Command {
     { name: 'spec-file', description: 'spec path, url, or context-name', required: false },
   ];
 
+  parser = new Parser();
+
   async run() {
     const { args, flags } = await this.parse(Validate); //NOSONAR
     const filePath = args['spec-file'];
@@ -29,6 +33,22 @@ export default class Validate extends Command {
       specWatcher({ spec: specFile, handler: this, handlerName: 'validate' });
     }
 
-    await validate(this, specFile, flags);
+    const result = await validate(this, specFile, flags);
+
+    try {
+      // Metrics recording.
+      const {document} = await this.parser.parse(specFile.text());
+      if (document !== undefined) {
+        const metadata = MetadataFromDocument(document);
+        metadata['success'] = true;
+        metadata['validation_result'] = result;
+        await this.recorder.recordActionExecution('validate', metadata);
+        await this.recorder.flush();
+      }
+    } catch (e: any) {
+      if (e instanceof Error) {
+        this.log(`Skipping submitting anonymous metrics due to the following error: ${e.name}: ${e.message}`);
+      }
+    }
   }
 }
