5d7ab9d765769625512fcf49268b76d6c8615cf6
Thijs Vermeir <thijsvermeir@gmail.com>
2023-10-29 09:21:40 +0100

    chore: prefer nullish coalescing operator

--- src/flags.ts
+++ src/flags.ts
@@ -4,6 +4,6 @@ export const watchFlag = (description?: string) => {
   return Flags.boolean({
     default: false,
     char: 'w',
-    description: description || 'Enable watch mode',
+    description: description ?? 'Enable watch mode',
   });
 };
