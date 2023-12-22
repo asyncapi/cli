5e63bddd89b538d61c6595ddb848fa1eb0ada8e1
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-12-22 20:22:52 +0530

    feat: add checksum

--- .github/workflows/deploy/chocolatey/tools/chocolateyinstall.ps1
+++ .github/workflows/deploy/chocolatey/tools/chocolateyinstall.ps1
@@ -14,11 +14,10 @@ $packageArgs = @{
 
   softwareName  = 'asyncapi-cli*'
 
-  # TODO :- Change the checksum and checksumType to match the checksums for the above file.
-  # checksum      = ''
-  # checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  # checksum64    = ''
-  # checksumType64= 'sha256' #default is checksumType
+  checksum      = '{{checksum}}'
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+  checksum64    = '{{checksum64}}'
+  checksumType64= 'sha256' #default is checksumType
 
   validExitCodes= @(0, 3010, 1641)
   silentArgs   = '/S'           # NSIS
