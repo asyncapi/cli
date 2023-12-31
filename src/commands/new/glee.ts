f81c02c7ad3e69272869a67f9b0965f7d9025782
AyushNautiyalDeveloper <ayush@betalectic.com>
2023-12-31 16:34:36 +0530

    Added the support for both template and local

--- src/commands/new/glee.ts
+++ src/commands/new/glee.ts
@@ -1,8 +1,12 @@
 import { Flags } from "@oclif/core";
+import { promises as fPromises } from "fs";
 import Command from "../../base";
+import { resolve, join } from "path";
+import fs from "fs-extra";
 
 export default class NewGlee extends Command {
   static description = "Creates a new Glee project";
+
   protected commandName = "glee";
 
   static flags = {
@@ -12,28 +16,89 @@ export default class NewGlee extends Command {
       description: "name of the project",
       default: "project",
     }),
+    template: Flags.string({
+      char: "t",
+      description: "template for the project",
+    }),
+    file: Flags.string({
+      char: "f",
+      description: "path of the file",
+      default: "asyncapi.yaml",
+    }),
   };
 
-  static args = [
-    {
-      name: "file",
-      description: "spec path, URL or context-name",
-      required: true,
-    },
-  ];
   async run() {
-    const { args, flags } = await this.parse(NewGlee); // NOSONAR
-    const { file } = args;
+    const { flags } = await this.parse(NewGlee); // NOSONAR
 
-    const gitrepo = "https://github.com/KhudaDad414/glee-generator-template";
-    try {
-      await this.config.runCommand("generate:fromTemplate", [
-        file,
-        gitrepo,
-        `--output=${flags.name}`,
-      ]);
-    } catch (error) {
-      console.log(error);
+    const { name: projectName, template, file } = flags;
+
+    const PROJECT_DIRECTORY = join(process.cwd(), projectName);
+    const GLEE_TEMPLATES_DIRECTORY = resolve(
+      __dirname,
+      "../../../assets/create-glee-app/templates/default",
+    );
+
+    console.log({ GLEE_TEMPLATES_DIRECTORY });
+    console.log({ file }, { template });
+    if (template) {
+      try {
+        await this.config.runCommand("generate:fromTemplate", [
+          file,
+          template,
+          `--output=${flags.name}`,
+        ]);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      try {
+        await fPromises.mkdir(PROJECT_DIRECTORY);
+      } catch (err: any) {
+        switch (err.code) {
+          case "EEXIST":
+            this.error(
+              `Unable to create the project. We tried to use "${projectName}" as the directory of your new project but it already exists (${PROJECT_DIRECTORY}). Please specify a different name for the new project. For example, run the following command instead:\n\n  asyncapi new ${this.commandName} --name ${projectName}-1\n`,
+            );
+            break;
+          case "EACCES":
+            this.error(
+              `Unable to create the project. We tried to access the "${PROJECT_DIRECTORY}" directory but it was not possible due to file access permissions. Please check the write permissions of your current working directory ("${process.cwd()}").`,
+            );
+            break;
+          case "EPERM":
+            this.error(
+              `Unable to create the project. We tried to create the "${PROJECT_DIRECTORY}" directory but the operation requires elevated privileges. Please check the privileges for your current user.`,
+            );
+            break;
+          default:
+            this.error(
+              `Unable to create the project. Please check the following message for further info about the error:\n\n${err}`,
+            );
+        }
+      }
+
+      try {
+        await fs.copy(GLEE_TEMPLATES_DIRECTORY, PROJECT_DIRECTORY);
+        await fPromises.rename(
+          `${PROJECT_DIRECTORY}/env`,
+          `${PROJECT_DIRECTORY}/.env`,
+        );
+        await fPromises.rename(
+          `${PROJECT_DIRECTORY}/gitignore`,
+          `${PROJECT_DIRECTORY}/.gitignore`,
+        );
+        await fPromises.rename(
+          `${PROJECT_DIRECTORY}/README-template.md`,
+          `${PROJECT_DIRECTORY}/README.md`,
+        );
+        this.log(
+          `Your project "${projectName}" has been created successfully!\n\nNext steps:\n\n  cd ${projectName}\n  npm install\n  npm run dev\n\nAlso, you can already open the project in your favorite editor and start tweaking it.`,
+        );
+      } catch (err) {
+        this.error(
+          `Unable to create the project. Please check the following message for further info about the error:\n\n${err}`,
+        );
+      }
     }
   }
 }
--- src/commands/new/template.ts
+++
@@ -1,4 +0,0 @@
-const template =
-  'import { GleeFunction, GleeFunctionEvent } from "@asyncapi/glee"; type PayloadType = {{payload}}; type GleeFunctionEventWithCustomPayload = GleeFunctionEvent & { payload?: PayloadType;}; const {{functionName}}: GleeFunction = async ( event: GleeFunctionEventWithCustomPayload,) => { return { reply: [{payload:`Hello from Glee! You said: "${event.payload}".`,},],};};export default {{functionName}}; ';
-
-export default template;
