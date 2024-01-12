ce3b64bd556362d38586e499e0aec89b780abc7a
AyushNautiyalDeveloper <ayush@betalectic.com>
2024-01-12 22:55:38 +0530

    removed the unwated files.

--- src/utils/functionTemplate.ts
+++
@@ -1,4 +0,0 @@
-const fileTemplate =
-  'import { GleeFunction, GleeFunctionEvent } from "@asyncapi/glee"; {{{payloadType}}} type GleeFunctionEventWithCustomPayload = GleeFunctionEvent & { payload?: PayloadType;}; const {{functionName}}: GleeFunction = async ( event: GleeFunctionEventWithCustomPayload,) => { return { reply: [{payload:`Hello from Glee! You said: "${event.payload}".`,},],};};export default {{functionName}}; ';
-
-export default fileTemplate;
