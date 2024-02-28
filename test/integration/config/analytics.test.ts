0f3da304724dad9f8ccf356885b18a5e2c799433
Pedro Ramos <pedroramosromero@gmail.com>
2024-02-28 15:55:25 +0100

    Solve linter issues

--- test/integration/config/analytics.test.ts
+++ test/integration/config/analytics.test.ts
@@ -4,48 +4,43 @@ import TestHelper from '../../helpers';
 const testHelper = new TestHelper();
 
 describe('config:analytics', () => {
-    afterEach(() => {
-        try {
-          testHelper.deleteAnalyticsConfigFile();
-        } catch (e: any) {
-            throw e;
-        }
-    });
-
-    describe('with disable flag', () => {
-      test
-        .stderr()
-        .stdout()
-        .command(['config:analytics', '--disable'])
-        .it('should show a successful message once the analytics are disabled', async (ctx, done) => {
-            expect(ctx.stdout).to.equal('Analytics disabled.\n');
-            expect(ctx.stderr).to.equal('');
-            done();
-        });
-    });
-
-    describe('with enable flag', () => {
-        test
-        .stderr()
-        .stdout()
-        .command(['config:analytics', '--enable'])
-        .it('should show a successful message once the analytics are enabled', (ctx, done) => {
-            expect(ctx.stdout).to.equal('Analytics enabled.\n');
-            expect(ctx.stderr).to.equal('');
-            done();
-        });
-    });
-
-    describe('with no flags', () => {
-        test
-        .stderr()
-        .stdout()
-        .command(['config:analytics'])
-        .it('should show informational message when no flags are used', (ctx, done) => {
-            expect(ctx.stdout).to.equal('\nPlease append the "--disable" flag to the command in case you prefer to disable analytics, or use the "--enable" flag if you want to enable analytics back again.\n\n');
-            expect(ctx.stderr).to.equal('');
-            done();
-        });
-    });
+  afterEach(() => {
+    testHelper.deleteAnalyticsConfigFile();
   });
-  
+
+  describe('with disable flag', () => {
+    test
+      .stderr()
+      .stdout()
+      .command(['config:analytics', '--disable'])
+      .it('should show a successful message once the analytics are disabled', async (ctx, done) => {
+        expect(ctx.stdout).to.equal('Analytics disabled.\n');
+        expect(ctx.stderr).to.equal('');
+        done();
+      });
+  });
+
+  describe('with enable flag', () => {
+    test
+      .stderr()
+      .stdout()
+      .command(['config:analytics', '--enable'])
+      .it('should show a successful message once the analytics are enabled', (ctx, done) => {
+        expect(ctx.stdout).to.equal('Analytics enabled.\n');
+        expect(ctx.stderr).to.equal('');
+        done();
+      });
+  });
+
+  describe('with no flags', () => {
+    test
+      .stderr()
+      .stdout()
+      .command(['config:analytics'])
+      .it('should show informational message when no flags are used', (ctx, done) => {
+        expect(ctx.stdout).to.equal('\nPlease append the "--disable" flag to the command in case you prefer to disable analytics, or use the "--enable" flag if you want to enable analytics back again.\n\n');
+        expect(ctx.stderr).to.equal('');
+        done();
+      });
+  });
+});
