80af106f3ed8eac7bc91f8b4202f69c25b1a5999
Pedro Ramos <pedroramosromero@gmail.com>
2024-04-30 20:09:46 +0200

    Fix failing tests

--- test/integration/config/analytics.test.ts
+++ test/integration/config/analytics.test.ts
@@ -7,7 +7,7 @@ describe('config:analytics', () => {
       .stdout()
       .command(['config:analytics', '--disable'])
       .it('should show a successful message once the analytics are disabled', async (ctx, done) => {
-        expect(ctx.stdout).to.equal('Analytics disabled.\n');
+        expect(ctx.stdout).to.equal('\nAnalytics disabled.\n\n');
         expect(ctx.stderr).to.equal('');
         done();
       });
@@ -19,7 +19,7 @@ describe('config:analytics', () => {
       .stdout()
       .command(['config:analytics', '--enable'])
       .it('should show a successful message once the analytics are enabled', (ctx, done) => {
-        expect(ctx.stdout).to.equal('Analytics enabled.\n');
+        expect(ctx.stdout).to.equal('\nAnalytics enabled.\n\n');
         expect(ctx.stderr).to.equal('');
         done();
       });
@@ -31,7 +31,7 @@ describe('config:analytics', () => {
       .stdout()
       .command(['config:analytics'])
       .it('should show informational message when no flags are used', (ctx, done) => {
-        expect(ctx.stdout).to.equal('\nPlease append the "--disable" flag to the command in case you prefer to disable analytics, or use the "--enable" flag if you want to enable analytics back again.\n\n');
+        expect(ctx.stdout).to.equal('\nPlease append the "--disable" flag to the command in case you prefer to disable analytics, or use the "--enable" flag if you want to enable analytics back again. In case you do not know the analytics current status, then you can append the "--status" flag to be aware of it.\n\n');
         expect(ctx.stderr).to.equal('');
         done();
       });
