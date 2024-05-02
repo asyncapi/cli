6f0f0dc21fda84c178677b207a70e4f26de3eb2f
Pedro Ramos <pedroramosromero@gmail.com>
2024-05-02 10:16:40 +0200

    Add requested change

--- src/commands/config/analytics.ts
+++ src/commands/config/analytics.ts
@@ -12,7 +12,7 @@ export default class Analytics extends Command {
     help: Flags.help({ char: 'h' }),
     disable: Flags.boolean({ char: 'd', description: 'disable analytics', default: false }),
     enable: Flags.boolean({ char: 'e', description: 'enable analytics', default: false }),
-    status: Flags.boolean({ char: 's', description: 'show analytics current status' }),
+    status: Flags.boolean({ char: 's', description: 'show current status of analytics' }),
 
   };
 
