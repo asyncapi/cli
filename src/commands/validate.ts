d490a500cc948f1f2c0b85616137897027af3991
AayushSaini101 <kumaraayush9810@gmail.com>
2024-05-20 15:37:20 +0530

    remove extra files and refract the code

--- src/commands/validate.ts
+++ src/commands/validate.ts
@@ -1,24 +1,40 @@
-import { Args } from '@oclif/core';
-import Command from '../core/base';
-import { validate, ValidateOptions, ValidationStatus } from '../core/parser';
-import { load } from '../core/models/SpecificationFile';
-import { specWatcher } from '../core/globals';
-import { validateFlags } from '../core/flags/validate.flags';
+import { Flags, Args } from '@oclif/core';
+import Command from '../base';
+import { calculateScore } from '../utils/scoreCalculator';
+import { validate, validationFlags, ValidateOptions, ValidationStatus } from '../parser';
+import { load } from '../models/SpecificationFile';
+import { specWatcher } from '../globals';
+import { watchFlag } from '../flags';
+import { parse} from '../parser';
 
 export default class Validate extends Command {
   static description = 'validate asyncapi file';
 
-  static flags = validateFlags();
+  static flags = {
+    help: Flags.help({ char: 'h' }),
+    watch: watchFlag(),
+    ...validationFlags(),
+    score: Flags.boolean({
+      
+      required: false,
+      default: false
+    }),
+  };
 
   static args = {
-    'spec-file': Args.string({description: 'spec path, url, or context-name', required: false}),
+    'spec-file': Args.string({ description: 'spec path, url, or context-name', required: false }),
   };
 
   async run() {
     const { args, flags } = await this.parse(Validate); //NOSONAR
+
     const filePath = args['spec-file'];
     const watchMode = flags.watch;
-
+    if (flags['score']) {
+      this.specFile = await load(filePath);
+      const { document } = await parse(this,this.specFile);
+      this.log(await calculateScore(document));
+    }
     this.specFile = await load(filePath);
     if (watchMode) {
       specWatcher({ spec: this.specFile, handler: this, handlerName: 'validate' });
