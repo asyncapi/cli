fbe755750dd5a4f846f0e7e43d03a4f8e14f41a3
Pedro Ramos <pedroramosromero@gmail.com>
2024-05-28 19:27:07 +0200

    Use a different path for analytics config file

--- test/integration/config/analytics.test.ts
+++ test/integration/config/analytics.test.ts
@@ -5,7 +5,7 @@ describe('config:analytics', () => {
     test
       .stderr()
       .stdout()
-      .command(['config:analytics', '--disable'])
+      .command(['ASYNCAPI_METRICS_CONFIG_PATH=./test/fixtures/.asyncapi-analytics', 'config:analytics', '--disable'])
       .it('should show a successful message once the analytics are disabled', async (ctx, done) => {
         expect(ctx.stdout).to.equal('\nAnalytics disabled.\n\n');
         expect(ctx.stderr).to.equal('');
@@ -17,7 +17,7 @@ describe('config:analytics', () => {
     test
       .stderr()
       .stdout()
-      .command(['config:analytics', '--enable'])
+      .command(['ASYNCAPI_METRICS_CONFIG_PATH=./test/fixtures/.asyncapi-analytics', 'config:analytics', '--enable'])
       .it('should show a successful message once the analytics are enabled', (ctx, done) => {
         expect(ctx.stdout).to.equal('\nAnalytics enabled.\n\n');
         expect(ctx.stderr).to.equal('');
@@ -29,7 +29,7 @@ describe('config:analytics', () => {
     test
       .stderr()
       .stdout()
-      .command(['config:analytics'])
+      .command(['ASYNCAPI_METRICS_CONFIG_PATH=./test/fixtures/.asyncapi-analytics', 'config:analytics'])
       .it('should show informational message when no flags are used', (ctx, done) => {
         expect(ctx.stdout).to.equal('\nPlease append the "--disable" flag to the command in case you prefer to disable analytics, or use the "--enable" flag if you want to enable analytics back again. In case you do not know the analytics current status, then you can append the "--status" flag to be aware of it.\n\n');
         expect(ctx.stderr).to.equal('');
@@ -41,7 +41,7 @@ describe('config:analytics', () => {
     test
       .stderr()
       .stdout()
-      .command(['config:analytics', '--status'])
+      .command(['ASYNCAPI_METRICS_CONFIG_PATH=./test/fixtures/.asyncapi-analytics', 'config:analytics', '--status'])
       .it('should show a different informational message depending on the analytics status', (ctx, done) => {
         expect(ctx.stdout).to.contain('\nAnalytics are ');
         expect(ctx.stderr).to.equal('');
