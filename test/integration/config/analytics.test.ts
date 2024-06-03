44adcdd7d237b4032a87cb7fe8ac327ae01bb686
Pedro Ramos <pedroramosromero@gmail.com>
2024-06-03 18:12:55 +0200

    Set up analytics config file properly for every test

--- test/integration/config/analytics.test.ts
+++ test/integration/config/analytics.test.ts
@@ -1,11 +1,22 @@
 import { expect, test } from '@oclif/test';
+import { fileCleanup } from '../../helpers';
+
+const analyticsConfigFilePath = './test/fixtures/.asyncapi-analytics';
 
 describe('config:analytics', () => {
+  beforeEach(() => {
+    process.env = Object.assign(process.env, { ASYNCAPI_METRICS_CONFIG_PATH: analyticsConfigFilePath });
+  });
+
+  afterEach(() => {
+    fileCleanup(analyticsConfigFilePath);
+  });
+
   describe('with disable flag', () => {
     test
       .stderr()
       .stdout()
-      .command(['ASYNCAPI_METRICS_CONFIG_PATH=./test/fixtures/.asyncapi-analytics', 'config:analytics', '--disable'])
+      .command(['config:analytics', '--disable'])
       .it('should show a successful message once the analytics are disabled', async (ctx, done) => {
         expect(ctx.stdout).to.equal('\nAnalytics disabled.\n\n');
         expect(ctx.stderr).to.equal('');
@@ -17,7 +28,7 @@ describe('config:analytics', () => {
     test
       .stderr()
       .stdout()
-      .command(['ASYNCAPI_METRICS_CONFIG_PATH=./test/fixtures/.asyncapi-analytics', 'config:analytics', '--enable'])
+      .command(['config:analytics', '--enable'])
       .it('should show a successful message once the analytics are enabled', (ctx, done) => {
         expect(ctx.stdout).to.equal('\nAnalytics enabled.\n\n');
         expect(ctx.stderr).to.equal('');
@@ -29,7 +40,7 @@ describe('config:analytics', () => {
     test
       .stderr()
       .stdout()
-      .command(['ASYNCAPI_METRICS_CONFIG_PATH=./test/fixtures/.asyncapi-analytics', 'config:analytics'])
+      .command(['config:analytics'])
       .it('should show informational message when no flags are used', (ctx, done) => {
         expect(ctx.stdout).to.equal('\nPlease append the "--disable" flag to the command in case you prefer to disable analytics, or use the "--enable" flag if you want to enable analytics back again. In case you do not know the analytics current status, then you can append the "--status" flag to be aware of it.\n\n');
         expect(ctx.stderr).to.equal('');
@@ -41,7 +52,7 @@ describe('config:analytics', () => {
     test
       .stderr()
       .stdout()
-      .command(['ASYNCAPI_METRICS_CONFIG_PATH=./test/fixtures/.asyncapi-analytics', 'config:analytics', '--status'])
+      .command(['config:analytics', '--status'])
       .it('should show a different informational message depending on the analytics status', (ctx, done) => {
         expect(ctx.stdout).to.contain('\nAnalytics are ');
         expect(ctx.stderr).to.equal('');
