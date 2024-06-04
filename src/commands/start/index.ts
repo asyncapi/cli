2b79f99426e67159d96d930b16cfa664277fbfd8
Samir AMZANI <samir.amzani@gmail.com>
2024-06-04 11:19:05 +0200

    feat: use core flags in start commands

--- src/commands/start/index.ts
+++ src/commands/start/index.ts
@@ -2,7 +2,7 @@ import Command from '../../core/base';
 import { Help } from '@oclif/core';
 
 export default class Start extends Command {
-  static description = 'Start asyncapi studio';
+  static description = '';
   async run() {
     const help = new Help(this.config);
     help.showHelp(['start', '--help']);
--- src/core/flags/start/api.flags.ts
+++
@@ -1,8 +0,0 @@
-import { Flags } from '@oclif/core';
-
-export const apiFlags = (defaultPort: number) => {
-  return {
-    help: Flags.help({ char: 'h' }),
-    port: Flags.integer({ char: 'p', description: 'port in which to start API', default: defaultPort }),
-  };
-};
