fbd63ec1c7ec8070260da06b7fa3622dd2a45855
Samir AMZANI <samir.amzani@gmail.com>
2024-06-04 11:20:26 +0200

    feat: use core flags in bundle command

--- src/core/flags/bundle.flags.ts
+++ src/core/flags/bundle.flags.ts
@@ -4,7 +4,8 @@ export const bundleFlags = () => {
   return {
     help: Flags.help({ char: 'h' }),
     output: Flags.string({ char: 'o', description: 'The output file name. Omitting this flag the result will be printed in the console.' }),
-    'reference-into-components': Flags.boolean({ char: 'r', description: 'Bundle the message $refs into components object.' }),
-    base: Flags.string({ char: 'b', description: 'Path to the file which will act as a base. This is required when some properties are to needed to be overwritten.' }),
+    base: Flags.string({ char: 'b', description: 'Path to the file which will act as a base. This is required when some properties need to be overwritten.' }),
+    baseDir: Flags.string({ char: 'd', description: 'One relative/absolute path to directory relative to which paths to AsyncAPI Documents that should be bundled will be resolved.' }),
+    xOrigin: Flags.boolean({ char: 'x', description: 'Pass this switch to generate properties "x-origin" that will contain historical values of dereferenced "$ref"s.' }),
   };
 };
