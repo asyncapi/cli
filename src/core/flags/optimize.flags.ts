de2dd4355d7a5afde7b9192fcef6734d4e81b796
helios2003 <ankitdash2019@gmail.com>
2024-06-24 09:25:09 +0530

    fixes in ignore flag

--- src/core/flags/optimize.flags.ts
+++ src/core/flags/optimize.flags.ts
@@ -7,6 +7,10 @@ export enum Optimizations {
   MOVE_ALL_TO_COMPONENTS='move-all-to-components',
 }
 
+export enum DisableOptimizations {
+  SCHEMA='schema',
+}
+
 export enum Outputs {
   TERMINAL='terminal',
   NEW_FILE='new-file',
@@ -17,6 +21,7 @@ export const optimizeFlags = () => {
   return {
     help: Flags.help({ char: 'h' }),
     optimization: Flags.string({char: 'p', default: Object.values(Optimizations), options: Object.values(Optimizations), multiple: true, description: 'select the type of optimizations that you want to apply.'}),
+    ignore: Flags.string({char: 'i', default: [], options: Object.values(DisableOptimizations), multiple: true, description: 'list of components to be ignored from the optimization process'}),
     output: Flags.string({char: 'o', default: Outputs.TERMINAL, options: Object.values(Outputs), description: 'select where you want the output.'}),
     'no-tty': Flags.boolean({ description: 'do not use an interactive terminal', default: false }),
   };
