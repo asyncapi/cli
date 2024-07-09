457cf0c128687a392d9c99a057c8139941160580
Zack_Aayush <60972989+AayushSaini101@users.noreply.github.com>
2024-07-09 19:44:52 +0530

    Update template.flags.ts

--- src/core/flags/new/template.flags.ts
+++ src/core/flags/new/template.flags.ts
@@ -24,6 +24,7 @@ export const templateFlags = () => {
         'Force writing of the generated files to given directory even if it is a git repo with unstaged files or not empty dir (defaults to false)',
     }),
     renderer: Flags.string({
+      char: 'r',
       default: 'react',
       description: 'Creating a template for particular engine, Its value can be either react or nunjucks.'
     })
