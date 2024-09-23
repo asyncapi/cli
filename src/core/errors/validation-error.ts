3ebe407c0edbcf37585320c29bcdcdaed9d5edd4
Vishal K <korada.vishal.phe22@itbhu.ac.in>
2024-09-23 20:41:01 +0530

    Added UI Changes to Optimize Command

--- src/core/errors/validation-error.ts
+++ src/core/errors/validation-error.ts
@@ -13,14 +13,13 @@ export class ValidationError extends Error {
       this.buildError(error.err);
     }
     if (error.type === 'invalid-file') {
-      this.message = `There is no file or context with name "${error.filepath}".`;
+      this.message = `❌ There is no file or context with the name "${error.filepath}". Please check the path and try again.`;
     }
     if (error.type === 'no-spec-found') {
-      this.message = 'Unable to perform validation. Specify what AsyncAPI file should be validated.\n\nThese are your options to specify in the CLI what AsyncAPI file should be used:\n- You can provide a path to the AsyncAPI file: asyncapi validate path/to/file/asyncapi.yml\n- You can also pass a saved context that points to your AsyncAPI file: asyncapi validate mycontext\n- In case you did not specify a context that you want to use, the CLI checks if there is a default context and uses it. To set default context run: asyncapi context use mycontext\n- In case you did not provide any reference to AsyncAPI file and there is no default context, the CLI detects if in your current working directory you have files like asyncapi.json, asyncapi.yaml, asyncapi.yml. Just rename your file accordingly.';
+      this.message = '❌ Unable to perform validation. Please specify which AsyncAPI file should be validated.';
     }
     this.name = 'ValidationError';
   }
-
   private buildError(err: any) {
     const errorsInfo: Array<string> = [];
 
@@ -37,8 +36,8 @@ export class ValidationError extends Error {
         const errorHasTitle = !!e.title;
         const errorHasLocation = !!e.location;
         /*
-        * All the conditions below are needed since validationErrors (from ParserError) come from Parser JS library, 
-        * so we cannot assure that all the fields or properties are always provided in the error. There might be cases 
+        * All the conditions below are needed since validationErrors (from ParserError) come from Parser JS library,
+        * so we cannot assure that all the fields or properties are always provided in the error. There might be cases
         * that even title is not provided.
         */
         if (errorHasTitle && errorHasLocation) {
