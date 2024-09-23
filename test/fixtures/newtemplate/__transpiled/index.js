fce91f919826d6754eaed074ae5ac98e7c4325b9
souvik <souvikde.ns@gmail.com>
2024-09-23 12:24:23 +0530

    Update index.js

--- test/fixtures/newtemplate/__transpiled/index.js
+++ test/fixtures/newtemplate/__transpiled/index.js
@@ -2,7 +2,7 @@
 
 require('source-map-support/register');
 var generatorReactSdk = require('@asyncapi/generator-react-sdk');
-var jsxRuntime = require('react/cjs/react-jsx-runtime.production.min');
+var jsxRuntime = require('/mnt/c/Users/Souvik/Documents/Programs/asyncapi/forks/cli/node_modules/@asyncapi/generator-react-sdk/node_modules/react/cjs/react-jsx-runtime.production.min.js');
 
 function index ({
   asyncapi,
