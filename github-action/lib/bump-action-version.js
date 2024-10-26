19fab83aa35b155ba427f62280372a9fe2bb4548
akshatnema <akshatnema.school@gmail.com>
2024-10-26 19:39:49 +0530

    added new plugin for semantic release of project

---
+++ github-action/lib/bump-action-version.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = async (pluginConfig, context) => {
+  const { nextRelease } = context;
+  const version = nextRelease.version;
+
+  const templatePath = path.resolve(__dirname, '../../', 'action-template.yml');
+  const outputPath = path.resolve(__dirname, '../../', 'action.yml');
+
+  try {
+    const templateContent = fs.readFileSync(templatePath, 'utf8');
+    const updatedContent = templateContent.replace(/\${ version }/g, version);
+    fs.writeFileSync(outputPath, updatedContent, 'utf8');
+    console.log(`Updated action.yml with version ${version}`);
+  } catch (error) {
+    console.error('Error updating action.yml:', error);
+    throw error;
+  }
+};
