daac2a62ee2d21dadc4f133bed5b95e4f53bb353
AayushSaini101 <kumaraayush9810@gmail.com>
2024-12-10 12:09:59 +0530

    Add the logic to add proxy in the termial

--- src/commands/config/proxy.ts
+++ src/commands/config/proxy.ts
@@ -7,8 +7,34 @@ export default class Proxy extends Command {
   static flags = proxyFlags();
 
   async run() {
-    const { args, flags } = await this.parse(Proxy); //NOSONAR
-    console.log(args);
-    console.log(flags);
+    const { flags } = await this.parse(Proxy); //NOSONAR
+    // Capturing flags
+    const https_proxy = flags['https_proxy'];
+    const http_proxy = flags['http_proxy'];
+    const no_proxy = flags['no_proxy'];
+
+    // Setting environment variables if flags are provided
+    if (http_proxy) {
+      process.env.http_proxy = http_proxy;
+      this.log(
+        'http_proxy has been succesfully added in the CLI} ✅',
+      );
+    }
+    if (https_proxy) {
+      process.env.https_proxy = https_proxy;
+      this.log(
+        'https_proxy has been succesfully added in the CLI} ✅',
+      );
+    }
+
+    // Removing all proxies if `--no_proxy` is provided
+    if (no_proxy) {
+      delete process.env.http_proxy;
+      delete process.env.https_proxy;
+
+      this.log(
+        'All the proxies has been removed from the CLI } ✅',
+      );
+    }
   }
 }
