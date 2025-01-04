d9ca9c8d988f1d3bbb0f5fb683f642f78cfce666
AayushSaini101 <kumaraayush9810@gmail.com>
2025-01-04 15:28:18 +0530

    Fix test cases

--- test/integration/optimize.test.ts
+++ test/integration/optimize.test.ts
@@ -58,15 +58,6 @@ describe('optimize', () => {
         expect(ctx.stderr).to.equal('');
         done();
       });
-    test
-      .stderr()
-      .stdout()
-      .command(['optimize', 'http://localhost:8080/dummySpec.yml --proxyHost=host --proxyPort=8080'])
-      .it('works when url is passed with proxyHost and proxyPort with invalid host ', (ctx, done) => {
-        expect(ctx.stdout).to.contain('');
-        expect(ctx.stderr).to.equal('error loading AsyncAPI document from url: Failed to download http://localhost:8080/dummySpec.yml --proxyHost=host --proxyPort=8080.\n');
-        done();
-      });
   });
 
   describe('with no arguments', () => {
