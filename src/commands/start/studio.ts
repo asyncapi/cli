df5a23a0f8c4a1f2d10830830a4b2bdbeb4b5158
lmalkam <lk21csb0a35@student.nitw.ac.in>
2025-01-15 12:15:30 +0530

    Make asyncapi start studio consistent with other commands

--- src/commands/start/studio.ts
+++ src/commands/start/studio.ts
@@ -2,18 +2,36 @@ import Command from '../../core/base';
 import { start as startStudio } from '../../core/models/Studio';
 import { load } from '../../core/models/SpecificationFile';
 import { studioFlags } from '../../core/flags/start/studio.flags';
+import { Args } from '@oclif/core';
+import { ValidationError } from '../../core/errors/validation-error';
 
 export default class StartStudio extends Command {
   static description = 'starts a new local instance of Studio';
 
   static flags = studioFlags();
 
+  static readonly args = {
+    'spec-file': Args.string({
+      description: 'spec path, url, or context-name',
+      required: true,
+    }),
+  };
+
   async run() {
-    const { flags } = await this.parse(StartStudio);
-    const filePath = flags.file || (await load()).getFilePath();
+    const { args, flags } = await this.parse(StartStudio);
+    const filePath = args['spec-file'];
     const port = flags.port;
-    
-    this.specFile = await load(filePath);
+
+    try {
+      this.specFile = await load(filePath);
+    } catch (err) {
+      this.error(
+        new ValidationError({
+          type: 'invalid-file',
+          filepath: filePath,
+        }),
+      );
+    }
     this.metricsMetadata.port = port;
 
     startStudio(filePath as string, port);
