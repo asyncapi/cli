c3dc0d95ea28fb2306d1bf564e955c2599cb0cbc
Adi-204 <adiboghawala@gmail.com>
2025-01-20 20:19:26 +0530

    feat: Remove -f flag from asyncapi start command

--- src/commands/start/studio.ts
+++ src/commands/start/studio.ts
@@ -2,20 +2,35 @@ import Command from '../../core/base';
 import { start as startStudio } from '../../core/models/Studio';
 import { load } from '../../core/models/SpecificationFile';
 import { studioFlags } from '../../core/flags/start/studio.flags';
+import { Args } from '@oclif/core';
 
 export default class StartStudio extends Command {
   static description = 'starts a new local instance of Studio';
 
   static flags = studioFlags();
 
-  async run() {
-    const { flags } = await this.parse(StartStudio);
-    const filePath = flags.file || (await load()).getFilePath();
-    const port = flags.port;
-    
-    this.specFile = await load(filePath);
-    this.metricsMetadata.port = port;
+  static args = {
+    'spec-file': Args.string({ description: 'spec path, url, or context-name', required: false }),
+  };
 
+  async run() {
+    const { args, flags } = await this.parse(StartStudio);
+    let filePath = args['spec-file'];
+    const port = flags.port;
+    if (!filePath) {
+      try {
+        filePath = ((await load()).getFilePath());
+        this.log(`Loaded specification from: ${filePath}`);
+      } catch (error) {
+        this.log('No file specified. Starting with an empty Studio.');
+      }
+    }
+    try {
+      this.specFile = await load(filePath);
+    } catch (error) {
+      filePath = "";
+    }
+    this.metricsMetadata.port = port;
     startStudio(filePath as string, port);
   }
 }
