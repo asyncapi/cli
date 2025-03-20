1bf2f1a7843b74290ad6b2b72222773d7f551228
neoandmatrix <tusharannand@gmail.com>
2025-03-20 18:59:02 +0530

    feat:made start studio command interactive

--- src/core/flags/start/studio.flags.ts
+++ src/core/flags/start/studio.flags.ts
@@ -5,5 +5,10 @@ export const studioFlags = () => {
     help: Flags.help({ char: 'h' }),
     file: Flags.string({ char: 'f', description: 'path to the AsyncAPI file to link with Studio', deprecated: true }),
     port: Flags.integer({ char: 'p', description: 'port in which to start Studio' }),
+    'no-interactive': Flags.boolean({
+      description: 'disables prompts for this command which asks for file path even if not passed via the arguments.',
+      required: false,
+      default: false,
+    }),
   };
 };
