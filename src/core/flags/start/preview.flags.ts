d17aa540766e13be9e8ea65127e33e29b449841f
Tushar <141230066+neoandmatrix@users.noreply.github.com>
2025-04-05 08:40:23 +0530

    feat: new command `asyncapi start preview` implemented. (#1753)
    * feat: preview command implemented.
    
    * fix: remove logs
    
    * chore: latest version of studio updated in the package.json
    
    * fix: port added
    
    * fix: handle bundle errors in preview start function
    
    * refactor: improve variable naming and error messages in preview command for cloud sonar .
    
    * refactor: enhance variable naming and improve error messages in startPreview function for cloud sonar.
    
    * fix: validate WebSocket connection origin in startPreview function.
    
    * refactor: update baseDir handling and improve file path resolution in startPreview function for windows.
    
    * refactor: update spec-file argument description and enhance startPreview function with additional parameters for base, baseDir, and xOrigin
    
    * fix: add error handling for server startup in startPreview function
    
    * feat: add detailed logging flag to startPreview function for improved error diagnostics
    
    * fix: sonar issues fixed.
    
    * fix: sonar issues fixed.
    
    * refactor: rename detailedLog flag to suppressLogs to show detailed logs by default and suppress if flag is passed.
    
    * refactor: update description of PreviewStudio command for clarity and accuracy
    
    * feat: add usage documentation for asyncapi start preview command
    
    * chore: changeset
    
    ---------
    
    Co-authored-by: Ashish Padhy <ashishpadhy1729@gmail.com>

---
+++ src/core/flags/start/preview.flags.ts
@@ -0,0 +1,12 @@
+import { Flags } from '@oclif/core';
+
+export const previewFlags = () => {
+  return {
+    help: Flags.help({ char: 'h' }),
+    port: Flags.integer({ char: 'p', description: 'port in which to start Studio in the preview mode' }),
+    base: Flags.string({ char: 'b', description: 'Path to the file which will act as a base. This is required when some properties need to be overwritten while bundling with the file.' }),
+    baseDir: Flags.string({ char: 'd', description: 'One relative/absolute path to directory relative to which paths to AsyncAPI Documents that should be bundled will be resolved.' }),
+    xOrigin: Flags.boolean({ char: 'x', description: 'Pass this switch to generate properties "x-origin" that will contain historical values of dereferenced "$ref"s.' }),
+    suppressLogs: Flags.boolean({char: 'l',description: 'Pass this to suppress the detiled error logs.',default: false})
+  };
+};
