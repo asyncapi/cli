0e8e3c1ce736f5d043a6cd4311ae2b8b604686ef
neoandmatrix <tusharannand@gmail.com>
2025-04-09 14:32:42 +0530

    feat: made the start studio command inteactive along with addition of a flag to disable prompt.

--- src/commands/start/studio.ts
+++ src/commands/start/studio.ts
@@ -3,6 +3,7 @@ import { start as startStudio } from '../../core/models/Studio';
 import { load } from '../../core/models/SpecificationFile';
 import { studioFlags } from '../../core/flags/start/studio.flags';
 import { Args } from '@oclif/core';
+import { isCancel, text, cancel } from '@clack/prompts';
 
 export default class StartStudio extends Command {
   static description = 'starts a new local instance of Studio';
@@ -16,13 +17,22 @@ export default class StartStudio extends Command {
   async run() {
     const { args, flags } = await this.parse(StartStudio);
 
+    let filePath = args['spec-file'] ?? flags.file;
+
+    let port = flags.port;
+
     if (flags.file) {
       this.warn('The file flag has been removed and is being replaced by the argument spec-file. Please pass the filename directly like `asyncapi start studio asyncapi.yml`');
     }
 
-    let filePath = args['spec-file'] ?? flags.file;
+    const isInteractive = !flags['no-interactive'];
 
-    const port = flags.port;
+    if (isInteractive && !filePath) {
+      const parsedArgs = await this.parseArgs({ filePath }, port?.toString());
+      filePath = parsedArgs.filePath;
+      port = parseInt(parsedArgs.port,10);
+    }
+
     if (!filePath) {
       try {
         filePath = ((await load()).getFilePath());
@@ -41,5 +51,43 @@ export default class StartStudio extends Command {
     }
     this.metricsMetadata.port = port;
     startStudio(filePath as string, port);
+  }
+
+  private async parseArgs(args:Record<string,any>,port?:string) {
+    const operationCancelled = 'Operation cancelled by the user.';
+    let askForPort = false;
+    let {filePath} = args;
+    if (!filePath) {
+      filePath = await text({
+        message: 'Enter the path to the AsyncAPI document',
+        defaultValue: 'asyncapi.yaml',
+        placeholder: 'asyncapi.yaml',
+        validate: (value) => {
+          if (!value) {return 'The path to the AsyncAPI document is required';}
+        },
+      });
+      askForPort = true;
+    }
+
+    if (isCancel(filePath)) {
+      cancel(operationCancelled);
+      this.exit();
+    }
+
+    if (!port && askForPort) {
+      port = await text({
+        message: 'Enter the port in which to start Studio',
+        defaultValue: '3210',
+        placeholder: '3210',
+        validate: (value) => (!value ? 'The port number is required' : undefined),
+      }) as string;
+    }
+
+    if (isCancel(port)) {
+      cancel(operationCancelled);
+      this.exit();
+    }
+
+    return { filePath, port: port ?? '3210' };
   }
 }
