2241bc16f529ff950b33ff82652b949318c10ca3
Tushar Anand <tusharannand@gmail.com>
2025-08-30 19:19:17 +0530

    chore: update to latest branch and fix tests
    Signed-off-by: Tushar Anand <tusharannand@gmail.com>

--- test/integration/validate.test.ts
+++ test/integration/validate.test.ts
@@ -3,16 +3,12 @@
 import path from 'path';
 import { test } from '@oclif/test';
 import { NO_CONTEXTS_SAVED } from '../../src/errors/context-error';
-import TestHelper, { closeStudioServer, createMockServer, stopMockServer } from '../helpers';
+import TestHelper, {createMockServer, stopMockServer } from '../helpers';
 import { expect } from '@oclif/test';
 
 const testHelper = new TestHelper();
 
 describe('validate', () => {
-  after(async () => {
-    await closeStudioServer();
-    await closeStudioServer(4321);
-  });
   describe('with file paths', () => {
     beforeEach(() => {
       testHelper.createDummyContextFile();
--- test/unit/utils/temp-dir.test.ts
+++ test/unit/utils/temp-dir.test.ts
@@ -1,8 +1,13 @@
 import { expect } from 'chai';
 import fs from 'fs';
 import { createTempDirectory, removeTempDirectory } from '../../../src/utils/temp-dir';
+import { closeStudioServer } from '../../helpers';
 
 describe('createTempDirectory() & removeTempDirectory()', () => {
+  after(async () => {
+    await closeStudioServer();
+    await closeStudioServer(4321);
+  });
   it('should create and then remove temp folder', async () => {
     // create dir
     const tempDir = await createTempDirectory();
