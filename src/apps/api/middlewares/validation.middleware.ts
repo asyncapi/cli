fcc864d5a1cddfcaecab9e90fc38e5c130607b31
Moderator <60972989+AayushSaini101@users.noreply.github.com>
2025-09-24 16:04:07 +0530

    Update src/apps/api/middlewares/validation.middleware.ts
    Co-authored-by: Fran Méndez <fmvilas@gmail.com>

--- src/apps/api/middlewares/validation.middleware.ts
+++ src/apps/api/middlewares/validation.middleware.ts
@@ -203,7 +203,7 @@ export async function validationMiddleware(
       __unstable: {
         resolver: {
           resolvers: [
-            // GitHub resolver for private repositories is automatically included by ValidationService
+            // Resolver for private repositories is automatically included by ValidationService
             // @TODO: Add Cookie Based Resolvers after migration and understanding some
             // details about how to use them in the new parser-js version.
           ],
