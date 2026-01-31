b81d3059ecf1051b5861413c2fc1aa7284765064
sahillllllllll-bit <sahilsingh59443@gmail.com>
2026-01-31 12:59:58 +0000

    fix(cli): improve empty content validation check

--- src/apps/cli/commands/generate/fromTemplate.ts
+++ src/apps/cli/commands/generate/fromTemplate.ts
@@ -64,7 +64,7 @@ export default class Template extends BaseGeneratorCommand {
         ? asyncapiInput
         : asyncapiInput?.toString?.() ?? '';
 
-    if (content.trim().length === 0) {
+    if(!content || !content.trim()){
       return this.error(
         new ValidationError({
           type: 'invalid-file',
